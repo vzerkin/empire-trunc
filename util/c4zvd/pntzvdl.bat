10cd9e038d74201878bf613d6a63e8b6c212a361
Mike Herman <mike.w.herman@gmail.com>
2002-10-10 14:31:29 +0200

    svn:60 ./ added

--- util/c4zvd/pntzvdl.bat
+++ util/c4zvd/pntzvdl.bat
@@ -4,10 +4,10 @@ rm $3
 rm tmp.dat
 rm pnt.zvd
 rm cur.zvd
-pntdat.exe $1 tmp.dat
-datzvd.exe tmp.dat pnt.zvd
-curzvd.exe $2 cur.zvd
+./pntdat.exe $1 tmp.dat
+./datzvd.exe tmp.dat pnt.zvd
+./curzvd.exe $2 cur.zvd
 echo #!zvview.exe >$3
 cat pnt.zvd >>$3
 cat cur.zvd >>$3
-zvv94l.exe $3
+./zvv94l.exe $3
