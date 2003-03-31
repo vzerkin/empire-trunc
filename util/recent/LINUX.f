88a9711272a4ea84f8066ba0bbaf88c6d54530b3
Mike Herman <mike.w.herman@gmail.com>
2003-03-31 20:51:25 +0200

    svn:72 adding RECENT

---
+++ util/recent/LINUX.f
@@ -0,0 +1,12 @@
+      SUBROUTINE TIMEIT(SECONDS)
+C=======================================================================
+C
+C     TIME SINCE START OF PROBLEM (BASE TIME) IN SECONDS
+C
+C=======================================================================
+      SAVE
+      DIMENSION TARRAY(2)
+      SECONDS=ETIME(TARRAY)
+      SECONDS=(TARRAY(1)+TARRAY(2))
+      RETURN
+      END
