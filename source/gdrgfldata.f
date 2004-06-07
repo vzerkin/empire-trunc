f7c63eddec45a64e47b989887a248b3abbf394b6
Roberto Capote <rcapotenoy@yahoo.com>
2004-06-07 12:34:01 +0200

    svn:156 C C    RCN, RIPL-2 files used instead of Plujko ones. C    The directory ../RIPL-2/gamma must be present C

--- source/gdrgfldata.f
+++ source/gdrgfldata.f
@@ -1,9 +1,13 @@
+$DEBUG
+Ccc   * $Author: Capote $
+Ccc   * $Date: 2004-06-07 10:34:01 $
+Ccc   * $Id: gdrgfldata.f,v 1.2 2004-06-07 10:34:01 Capote Exp $
 C
       SUBROUTINE GDRGFLDATA(Znucleus, Anucleus)
 C
 C    ********************************************************************
 C    *                                                                  *
-C    * Assignment of the GDR parameters and parametrs of the GFL model  *
+C    * Assignment of the GDR and GFL model parameters                   *
 C    * to nucleus with atomic number 'Znucleus'and mass number'Anucleus'*  .                                                   *
 C    *                                                                  *
 C    *  The parameters are put in the following COMMON's:               *
@@ -36,7 +40,7 @@ C    *                     parameter 'beta' associated                  *
 C    *                     with nuclear quadrupole moment,              *
 C    *  S2Plus(=(E2+)*beta**2)= product of first-excited                *
 C    *                          2+ state energy(in MeV)                 *
-C    *                          deforamation parameter                  *
+C    *                          deformation parameter                   *
 C    *                                                                  *
 C    *  Setting the GDR parameters                                      *
 C    *  --------------------------                                      *
@@ -72,16 +76,22 @@ C       with  in global.h)
       DOUBLE PRECISION a0, a3, aann, alambda, alpha2, b0, CS0, CS1, CS2,
      &                 csaa, csb, EG0, EG1, EG2, ea, eb, GW0, GW1, GW2,
      &                 hbetagfl, henergygfl, BETagfl2, S2Plusgfl
+      DOUBLE PRECISION etmp,fjtmp,btmp
+	INTEGER nntmp,natmp,MAXgdr
+	Parameter (MAXgdr=6000)
 C----Plujko_new
 Cb      DOUBLE PRECISION halpha2, hcs1, hcs2, he1, he2, hgw1, hgw2, pi, z,
 Cb     &                 betagfl, energygfl, Znucleus, Anucleus
       DOUBLE PRECISION halpha2, hcs1, hcs2, he1, he2, hgw1, hgw2, pi,
-     &              zz, betagfl, energygfl, Znucleus, Anucleus
+     &              zz, betagfl, energygfl, Znucleus, Anucleus,
+     &              he1t(MAXgdr), hgw1t(MAXgdr), etat(MAXgdr), 
+     &              he2t(MAXgdr), hgw2t(MAXgdr), etaeps
       INTEGER  Key_GDRGFL, Key_shape
       COMMON /GSA/ Key_shape, Key_GDRGFL
 C----Plujko_new(End)
       INTEGER i, ka, keyload, kz, n, nana, nanz, NG, nna, nng, nnz,
-     &        nzram, naram, numram, keyram, keyalpa, kaa, kzz
+     &        nzram, naram, numram, keyram, keyalpa, kaa, kzz,
+	&        nnzt(MAXgdr), nnat(MAXgdr), nngt(MAXgdr)
 C
       COMMON /PARGDR/  EG1, GW1, CS1, EG2, GW2, CS2, NG
       COMMON /GFLPARAM/ BETagfl2, S2Plusgfl
@@ -99,46 +109,64 @@ Cb     &          henergygfl(700), nzram(700), naram(700)
      &        nana(9000), halpha2(9000), hbetagfl(700),
      &        henergygfl(700), nzram(700), naram(700),numram
       DATA pi/3.141592654D0/
-Cb    DATA pi/3.141592654D0/, keyload/0/, keyalpa/0/
-Cb    DATA kzz/0/, kaa/0/
 C----Plujko_new(End)
       kz = Znucleus + 0.001
       ka = Anucleus + 0.001
-      IF(kz.EQ.kzz .AND. ka.EQ.kaa) GOTO 99999
+      IF(kz.EQ.kzz .AND. ka.EQ.kaa) return
       kzz=kz
       kaa=ka
       IF(keyload.NE.1)THEN
-         keyload = 1
-         OPEN(81, FILE = 'beijingn.dat', STATUS = 'old')
-         REWIND 81
-         READ(81, 99001)
-99001    FORMAT(/)
-         DO i = 1, 270
-            READ(81, 99002, END = 50, ERR = 50)nnz(i), nna(i), nng(i),
-     &           he1(i), hcs1(i), hgw1(i), he2(i), hcs2(i), hgw2(i)
-99002       FORMAT(i3, 3x, i4, 3x, i3, 2(2F7.3, f6.3))
-         ENDDO
- 50      OPEN(82, FILE = 'deflib.dat', STATUS = 'old')
-         REWIND 82
-         READ(82, 99003)
-99003    FORMAT(////)
-         DO i = 1, 9000
-            READ(82, 99004, END = 100, ERR = 100)nanz(i), nana(i),
-     &           halpha2(i)
-99004       FORMAT(2I4, f7.3)
-         ENDDO
- 100     OPEN(84, FILE = 'defeff.dat', STATUS = 'old')
-         REWIND 84
-         READ(84, '(///////////)')
-         numram = 0
-         DO i = 1, 700
-            READ(84, 99005, END = 200, ERR = 200)nzram(i), naram(i),
-     &           henergygfl(i), hbetagfl(i)
-99005       FORMAT(I4, I4, f13.5, f10.4)
-            numram = i
-         ENDDO
+        keyload = 1
+C       README file format (2I4,1X,A2,2I3,6F7.2,2X,A5)
+        OPEN(81, FILE = '../RIPL-2/gamma/gdr-parameters-exp.dat', 
+     &                                       STATUS = 'old',ERR=3500)
+        READ(81,'(///)') ! Skipping first 4 title lines
+        DO i = 1, 270
+           READ(81,'(2I4, 1x,2x,3x, i3, 6F7.2)', END = 40, 
+     & 		  ERR = 40) nnz(i), nna(i), nng(i),
+     &          he1(i), hcs1(i), hgw1(i), he2(i), hcs2(i), hgw2(i)
+        ENDDO
+40	  CLOSE(81)
+C       README file format (2i4,1x,a2,f7.3,4f7.2)
+41      OPEN(81, FILE = '../RIPL-2/gamma/gdr-parameters-theor.dat', 
+     &                                       STATUS = 'old',ERR=3550)
+        READ(81,'(///)') ! Skipping first 4 title lines
+        DO i = 1, MAXgdr
+           READ(81,'(2I4, 1x,2x, f7.3, 4F7.2)', END = 50, 
+     & 		  ERR = 50) nnzt(i), nnat(i), etat(i), 
+     &          he1t(i), hgw1t(i), he2t(i), hgw2t(i)
+	     nngt(i)=2
+	     if(he1t(i).eq.he2t(i)) nngt(i)=1
+        ENDDO
+50      CLOSE(81)
+51      OPEN(82, FILE = '../data/deflib.dat', STATUS = 'old',ERR=3600)
+        READ(82, '(////)')  ! Skipping first 5 title lines
+        DO i = 1, 9000
+           READ(82, '((2I4, f7.3))', END = 100, ERR = 100)
+     &	            nanz(i), nana(i), halpha2(i)
+        ENDDO
+100     CLOSE(82)
+C       OPEN(84, FILE = 'defeff.dat', STATUS = 'old')
+101     OPEN(84, FILE = 
+     &       '../RIPL-2/optical/om-data/om-deformations.dat',
+     &       STATUS = 'old',ERR=3700)
+        READ(84,'(///)') ! Skipping first 4 title lines
+        numram = 0
+        DO i = 1, 700
+C       README file format (2i4,1x,a2,1x,f10.6,1x,f4.1,i3,i2,1x,f10.6,2x,a13)
+            READ(84,'(2I4,4x,f10.6,1x,f4.1,6x,f10.6)', END = 200, 
+     &      	  ERR = 200) nntmp,natmp,etmp,fjtmp,btmp
+C           Selecting only 2+ states
+	      if(abs(fjtmp-2.d0).gt.0.0001) cycle 
+	      numram=numram+1 
+            nzram(numram)=nntmp
+		  naram(numram)=natmp
+	      henergygfl(numram)=etmp
+		  hbetagfl(numram)=btmp
+        ENDDO
+200     CLOSE(84)
       ENDIF
- 200  n = ka-kz
+201   n = ka-kz
       zz = kz
       aann = ka
       a3 = aann**0.3333333
@@ -146,10 +174,10 @@ C----Plujko_new(End)
       GW0 = 0.026*EG0**1.91
       CS0 = 1.2*120.*n*zz/(aann*pi*GW0)
 C----Plujko_new
-        IF (Key_GDRGFL.NE.0) THEN
+      IF (Key_GDRGFL.NE.0) THEN
 C----Plujko_new(END)
-      DO i = 1, 270
-         IF(kz.EQ.nnz(i) .AND. ka.EQ.nna(i))THEN
+        DO i = 1, 270
+          IF(kz.EQ.nnz(i) .AND. ka.EQ.nna(i))THEN
             NG = nng(i)
             EG1 = he1(i)
             CS1 = hcs1(i)
@@ -158,10 +186,32 @@ C----Plujko_new(END)
             CS2 = hcs2(i)
             GW2 = hgw2(i)
             GOTO 400
-         ENDIF
-      ENDDO
+          ENDIF
+        ENDDO
+C       If GDR parameters not found they are going to be retrieved from 
+C       the RIPL-2 Goriely theoretical values
+        DO i = 1, MAXgdr
+          IF(kz.EQ.nnzt(i) .AND. ka.EQ.nnat(i))THEN
+            NG = nngt(i)
+            EG1 = he1t(i)
+            CS1 = CS0
+            GW1 = hgw1t(i)
+            EG2 = he2t(i)
+C           CS2 = hcs2t(i)
+            GW2 = hgw2t(i)
+	      etaeps = etat(i)
+            IF(abs(etaeps-1.d0).GT.0.0001)THEN
+C             *************************************************
+C             *Global GDR parameterization for deformed nuclei*
+C             *(classical sum rule with correction)           *
+C             *************************************************
+              CS1 = CS0/3.
+              CS2 = CS0*2./3.
+            ENDIF
+          ENDIF
+        ENDDO
 C----Plujko_new
-        ENDIF
+      ENDIF
 C----Plujko_new(END)
 C     ***********************************************************
 C     *Setting the deformation parameter from "deflib.dat" file *
@@ -228,11 +278,13 @@ C              *'BETagfl2=beta**2' and  'S2Plus=(E2+)*beta**2'  -------        *
 C              *parameters of the GFL model[BETagfl2=beta; ENErgygfl=E2+(MeV)] *
 C              *****************************************************************
                BETagfl2 = betagfl**2
-               energygfl = henergygfl(i)*0.001
+C              energygfl = henergygfl(i)*0.001
+               energygfl = henergygfl(i)
+C              RIPL-2 energies in MeV, RCN 06/2004 
                S2Plusgfl = BETagfl2*energygfl
             ENDIF
          ENDIF
-         IF(keyram.EQ.1)GOTO 99999
+         IF(keyram.EQ.1) return
       ENDDO
 C     ***********************************************************
 C     * Global parametrization for 'S2Plus=(E2+)*beta**2'       *
@@ -252,7 +304,23 @@ C     ***********************************************************
          ENDDO
          alpha2 = 0.
       ENDIF
- 500  betagfl = 1.5853*ABS(alpha2)
-      BETagfl2 = betagfl**2
+ 500  BETagfl = 1.5853*ABS(alpha2)
+      BETagfl2 = BETagfl**2
       S2Plusgfl = 217.156/aann**2
-99999 END
+      RETURN
+3500  WRITE(6,'(1x,A14,A39,A43)') 'WARNING: FILE ',
+     &           '../RIPL-2/gamma/gdr-parameters-exp.dat',
+     &           ' not FOUND, theoretical RIPL-2 will be used'
+      GOTO 41
+3550  WRITE(6,'(1x,A14,A41,A35)') 'WARNING: FILE ',
+     &           '../RIPL-2/gamma/gdr-parameters-theor.dat',
+     &           ' not FOUND, DEFAULT GDR VALUES USED'
+      GOTO 51
+3600  WRITE(6,'(1x,A14,A18,A43)') 'WARNING: FILE ','../data/deflib.dat',	
+     &           ' not FOUND, DEFAULT deformation VALUES USED'
+      GOTO 101
+3700  WRITE(6,'(1x,A14,A45,A54)') 'WARNING: FILE ',	
+     &       '../RIPL-2/optical/om-data/om-deformations.dat',
+     &           ' not FOUND, DEFAULT dynamical deformation VALUES USED'
+	GOTO 201 
+      END
