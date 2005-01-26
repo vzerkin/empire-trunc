27cb7f7a5c94bf1caecfda737719f98645e16dad
Mike Herman <mike.w.herman@gmail.com>
2005-01-26 17:38:19 +0100

    svn:401 reverted the last change but set 'kase' to 0

--- source/MSD-orion.f
+++ source/MSD-orion.f
@@ -1,6 +1,6 @@
 Ccc   * $Author: herman $
-Ccc   * $Date: 2005-01-25 22:39:06 $
-Ccc   * $Id: MSD-orion.f,v 1.7 2005-01-25 22:39:06 herman Exp $
+Ccc   * $Date: 2005-01-26 16:38:19 $
+Ccc   * $Id: MSD-orion.f,v 1.8 2005-01-26 16:38:19 herman Exp $
 C
 C     Surface real OMP part (for DOM potentials) included
 C     but contribution to form factor is ignored (see subroutine FFCAL)
@@ -569,11 +569,12 @@ C--------transfer cross sections onto XWR* matrices
          ENDDO
       ENDIF   !on removing compressional form factor for dl=0
 C-----write results to TAPE15
+      kase = 0
       DO nlr = 1, 2
-C        IF(kase.NE.2)THEN
-C           WRITE(15, *)(xwr1(n, nlr), n = 1, n1wx)
-C           IF(NCHanl.EQ.2)GOTO 100
-C        ENDIF
+         IF(kase.NE.2)THEN
+            WRITE(15, *)(xwr1(n, nlr), n = 1, n1wx)
+            IF(NCHanl.EQ.2)GOTO 100
+         ENDIF
          WRITE(15, *)(xwr2(n, nlr), n = 1, n2mx)
  100  ENDDO
 99014 FORMAT(21X, 'VSX,DVX,WSX,WSF,VSO,WSO', 18X, '=', 6F8.3/21X,
