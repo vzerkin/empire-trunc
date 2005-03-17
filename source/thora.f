b262a4b0f32ea581eafcfa974047792472c15c25
Roberto Capote <rcapotenoy@yahoo.com>
2005-03-17 11:46:07 +0100

    svn:554 RCN, THORA routine (compatible with g77,LAHEY,MSFORTRAN,g95) It is F90 standard

---
+++ source/thora.f
@@ -0,0 +1,50 @@
+      SUBROUTINE THORA(IOUT)
+C     R. Capote, March 2005
+C     GIVES THE TIME ELAPSED SINCE THE FIRST CALL
+C      (Elapsed time must be less than one month)
+C     (g77, g95, LAHEY, and MS FORTRAN compatible)
+C
+      REAL BEGTIM,ENDTIM,BEGDAY,ENDDAY,DIFTIM,DIFTI1
+      INTEGER IOUT
+      CHARACTER*8 DATE
+      CHARACTER*10 TIME
+      CHARACTER*5 ZONE
+      INTEGER DT
+      DIMENSION DT(8)
+      LOGICAL NEVER_CALLED
+      DATA NEVER_CALLED/.TRUE./
+      SAVE BEGTIM,BEGDAY,NEVER_CALLED
+
+      IF (NEVER_CALLED) then
+        NEVER_CALLED = .FALSE.
+        CALL DATE_AND_TIME (DATE,TIME,ZONE,DT)
+        BEGDAY=DT(3)
+        BEGTIM=DT(5)*3600+DT(6)*60+DT(7)+DT(8)/1000.
+        WRITE(IOUT,1001) time(1:2),time(3:4),time(5:6),
+     >                   DATE(7:8),DATE(5:6),DATE(1:4)
+        WRITE(*   , 101) time(1:2),time(3:4),time(5:6),
+     >                   DATE(7:8),DATE(5:6),DATE(1:4)
+      ELSE
+        CALL DATE_AND_TIME (DATE,TIME,ZONE,DT)
+        ENDTIM=DT(5)*3600+DT(6)*60+DT(7)+DT(8)/1000.
+        ENDDAY=DT(3)
+        ENDTIM = ENDTIM +  (ENDDAY-BEGDAY)*86400.
+        DIFTIM=(ENDTIM-BEGTIM)/60.
+        DIFTI1=(DIFTIM-INT(DIFTIM))*60.
+        WRITE(IOUT,1002) time(1:2),time(3:4),time(5:6),
+     >   DATE(7:8),DATE(5:6),DATE(1:4),INT(DIFTIM),NINT(DIFTI1)
+        WRITE(*   ,1002) time(1:2),time(3:4),time(5:6),
+     >   DATE(7:8),DATE(5:6),DATE(1:4),INT(DIFTIM),NINT(DIFTI1)
+      ENDIF
+
+      RETURN
+ 1001 FORMAT
+     >(/22X,' START TIME: ',A2,':',A2,'.',A2,' (',A2,'-',A2,'-',A4,')'/)
+  101 FORMAT
+     >(/' START TIME: ',A2,':',A2,'.',A2,' (',A2,'-',A2,'-',A4,')'/)
+ 1002 FORMAT
+     >(/ 1X,' CURRENT TIME: ',A2,':',A2,'.',A2,' (',A2,'-',A2,'-',A4,')'
+     > //1X,' CALCULATION TIME: ',I3,' MIN ',I2,' SEC'/)
+C====================================================================
+      END
+
