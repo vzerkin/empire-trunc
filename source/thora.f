81daff7b3cc68120e88a99a23119ce10eccbbb4d
Mike Herman <mike.w.herman@gmail.com>
2005-03-18 17:51:01 +0100

    svn:561 changed printing

--- source/thora.f
+++ source/thora.f
@@ -1,6 +1,6 @@
-Ccc   * $Author: Capote $
-Ccc   * $Date: 2005-03-17 19:48:16 $
-Ccc   * $Id: thora.f,v 1.2 2005-03-17 19:48:16 Capote Exp $
+Ccc   * $Author: herman $
+Ccc   * $Date: 2005-03-18 16:51:01 $
+Ccc   * $Id: thora.f,v 1.3 2005-03-18 16:51:01 herman Exp $
       SUBROUTINE THORA(IOUT)
 C
 C     R. Capote, March 2005
@@ -28,8 +28,8 @@ C
         BEGTIM=DT(5)*3600+DT(6)*60+DT(7)+DT(8)/1000.
         WRITE(IOUT,1001) time(1:2),time(3:4),time(5:6),
      >                   DATE(7:8),DATE(5:6),DATE(1:4)
-        WRITE(*   , 101) time(1:2),time(3:4),time(5:6),
-     >                   DATE(7:8),DATE(5:6),DATE(1:4)
+c       WRITE(*   , 101) time(1:2),time(3:4),time(5:6),
+c    >                   DATE(7:8),DATE(5:6),DATE(1:4)
       ELSE
         CALL DATE_AND_TIME (DATE,TIME,ZONE,DT)
         ENDTIM=DT(5)*3600+DT(6)*60+DT(7)+DT(8)/1000.
@@ -37,20 +37,21 @@ C
         ENDTIM = ENDTIM +  (ENDDAY-BEGDAY)*86400.
         DIFTIM=(ENDTIM-BEGTIM)/60.
         DIFTI1=(DIFTIM-INT(DIFTIM))*60.
-        WRITE(IOUT,1002) time(1:2),time(3:4),time(5:6),
-     >   DATE(7:8),DATE(5:6),DATE(1:4),INT(DIFTIM),NINT(DIFTI1)
-        WRITE(*   ,1002) time(1:2),time(3:4),time(5:6),
-     >   DATE(7:8),DATE(5:6),DATE(1:4),INT(DIFTIM),NINT(DIFTI1)
+        WRITE(IOUT,1002) 
+c    >     time(1:2),time(3:4),time(5:6), DATE(7:8),DATE(5:6),DATE(1:4),
+     >     INT(DIFTIM),NINT(DIFTI1)
+c       WRITE(*   ,1002) time(1:2),time(3:4),time(5:6),
+c    >   DATE(7:8),DATE(5:6),DATE(1:4),INT(DIFTIM),NINT(DIFTI1)
       ENDIF
 
       RETURN
  1001 FORMAT
-     >(/22X,' START TIME: ',A2,':',A2,'.',A2,' (',A2,'-',A2,'-',A4,')'/)
+     >(/1X,'Start time: ',A2,':',A2,'.',A2,' (',A2,'-',A2,'-',A4,')'/)
   101 FORMAT
-     >(/1x,' START TIME: ',A2,':',A2,'.',A2,' (',A2,'-',A2,'-',A4,')'/)
- 1002 FORMAT
-     >(/ 1X,' CURRENT TIME: ',A2,':',A2,'.',A2,' (',A2,'-',A2,'-',A4,')'
-     > //1X,' CALCULATION TIME: ',I3,' MIN ',I2,' SEC'/)
+     >(/1x,'Start time: ',A2,':',A2,'.',A2,' (',A2,'-',A2,'-',A4,')'/)
+ 1002 FORMAT(
+c    > 1X,'Current time: ',A2,':',A2,'.',A2,' (',A2,'-',A2,'-',A4,')'/
+     > 1X,' Calculation time: ',I3,' min ',I2,' s')
 C====================================================================
       END
 
