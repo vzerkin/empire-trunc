b8ea43fb7a0dcd4ca8f34560cb7d292d9c66394e
Mike Herman <mike.w.herman@gmail.com>
2006-01-02 07:20:27 +0100

    svn:953 minor

--- source/covariance.f
+++ source/covariance.f
@@ -1,7 +1,7 @@
-Ccc   * $Author: 
-Ccc   * $Date: 
-Ccc   * $Id: 
-      program COVARIANCE
+Ccc   * $Author: herman $ 
+Ccc   * $Date: 2006-01-02 06:12:18 $
+Ccc   * $Id: covariance.f,v 1.5 2006-01-02 06:12:18 herman Exp $
+      PROGRAM COVARIANCE
       integer*4 PIPE,itmp,i1,i2
       character*80 command
       character*4 crun
@@ -14,8 +14,6 @@ C     write(*,*) 'Number of sampling runs?'
          write(crun,'(I4.4)') i
          CALL system('mv OUTPUT.DAT OUT'//crun)
          CALL system('mv LIST.DAT LST'//crun)
-C        CALL system("rm ../TL/000001_090232*.*")	 
-C        CALL system("rm ../TL/000001_090231*.*")	 	 
          open(10,file='R250SEED.DAT',status='OLD')
          read(10,*) i1,i2
          write(*,'(1x,A6,i12,1x,i12/)') 'SEEDS:',i1,i2
