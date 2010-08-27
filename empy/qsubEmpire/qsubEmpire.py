6f37dae5884478e4a602375c522165c0fbfecfe8
Caleb Mattoon <mattoon1@llnl.gov>
2010-08-27 20:47:28 +0200

    svn:1831 Putting qsubEmpire under version control

---
+++ empy/qsubEmpire/qsubEmpire.py
@@ -0,0 +1,311 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+<qsubEmpire.py>
+
+Created by Caleb Mattoon on 2009-12-10.
+Copyright (c) 2009 __nndc.bnl.gov__. All rights reserved.
+
+Run Empire on cluster using qsub
+split the input into multiple files, one per incident energy. Submit each file
+using qsub
+
+'sensitivity.py' (for creating sensitivity matrix) depends on this
+"""
+
+import os
+import sys
+import shutil
+from empy import bash
+
+def parseInput(inputFile):
+    """
+    EMPIRE input has three sections:
+        required header including first energy, 10 lines
+        options (select optical model, etc) terminated by 'GO'
+        incident energies, also including energy-dependant options
+    
+    inputFile is the path to an empire input
+    returns: three sections (header, options, (energy,opts)) as lists
+    """
+    fin = open(inputFile,"r")
+    
+    header = []     # required header
+    options = []    # options at start
+    energies = []   # list of energies
+    extraopts = []  # energy-dependant options
+
+    # every file should have 10 lines of header
+    for i in range(10):
+        header.append(fin.next())
+    assert 'NUMBER OF L.I. TO BE EMITTED' in header[-1],\
+            "Must use new-style input!"
+    
+    # read in options:
+    for line in fin:
+        if line[:1] in ('*','!','#'):
+            # skip comments
+            continue
+        if line[:2] == "GO":
+            # done with options
+            options.append(line)
+            break
+        # otherwise just add line to options:
+        options.append(line)
+    
+    # now read in energies:
+    tmp = []
+    for line in fin:
+        if line[:2] == '-1':
+            # done with energies
+            break
+        elif line[:1] in ('*','!','#'):
+            # skip comments
+            continue
+        elif line[:1] == '$':
+            tmp.append(line)
+        else:
+            # default case, new energy
+            energies.append(line)
+            extraopts.append(tmp[:])    # copy of tmp
+            #tmp = []
+    
+    fin.close()
+    return header, options, zip(energies,extraopts)
+
+
+def runInput(inputFile, clean=False):
+    """
+    1) creates a set of new input files, each in its own directory. 
+    Each file has the same header and options as original, plus a single energy
+    (may also include energy-dependant options)
+    2) runs all inputs using qsub
+    
+    If clean==True we only save the .xsc file (reduces network traffic,
+    best for sensitivity calculation)
+    """
+    from os.path import join
+    
+    path = getPath(inputFile)
+    proj = os.path.basename(inputFile).split('.')[0]
+    
+    header, options, energies = parseInput(inputFile)
+    
+    firsterr = True # reduce warnings if directories already exist
+    for (energy,energy_dep_opt) in energies[::-1]:
+        """
+        make a new directory with new input file, and execute empire
+        starting with largest energies may help with speed
+        """
+        name = proj+"-"+energy.strip()
+        try:
+            os.mkdir(join(path,name))
+        except OSError, e:
+            if firsterr:
+                print ("Using existing TLs!")
+                firsterr = False
+        fnew = open(join(path,name,proj+".inp"), "w")
+        
+        for l in header:
+            fnew.write(l)
+        for o in options:
+            fnew.write(o)
+        for o in energy_dep_opt:
+            fnew.write(o)
+        fnew.write(energy)
+        fnew.write("-1\n")
+        fnew.close()
+        
+        # copy other inputs as well, if available (no error message otherwise)
+        bash.cp(join(path,proj+"-inp.fis"),join(path,name),False)
+        bash.cp(join(path,proj+".lev"),join(path,name),False)
+        bash.cp(join(path,proj+"-lev.col"),join(path,name),False)
+        bash.cp(join(path,proj+"-omp.dir"),join(path,name),False)
+        bash.cp(join(path,proj+"-omp.ripl"),join(path,name),False)
+        
+        log = join( path,"empire_%s.log" % (name) )
+        dir = join( path,name )
+        if clean:
+            cmd = "qsub -N emp_%sMeV -o %s -l ncpus=1 " +\
+                    "-v dir=%s,file=%s,energy=%s,clean=Y ~/bin/runEmpire.sh"
+        else:
+            cmd = "qsub -N emp_%sMeV -o %s -l ncpus=1 " +\
+                    "-v dir=%s,file=%s,energy=%s ~/bin/runEmpire.sh"
+        
+        cmd = cmd % (energy.strip(), log, dir, proj, energy.strip())
+        #print cmd
+        os.system(cmd)
+
+
+def reconstruct(inputFile):
+    """
+    after running in multiple directories, 
+    reconstruct full .xsc and .out files
+    """
+    def readPastOneEnergy(fin,i,end=False):
+        # helper: read file until we arrive at next incident energy
+        # or end of file:
+        # if end == True, go straight to the end
+        while True:
+            line = fin[i]
+            if end:
+                if " CALCULATIONS COMPLETED SUCCESSFULLY" in line:
+                    break
+            else:
+                # read to next incident energy:
+                if "REACTION" in line:
+                    break
+            i += 1
+        return i
+    
+    path = getPath(inputFile)
+    proj = os.path.basename(inputFile).split('.')[0]
+    h,o,energies = parseInput(inputFile)
+    
+    # assume enlist is in order:
+    enlist = [e[0].strip() for e in energies]
+    
+    name = os.path.join(path,proj+"-"+enlist[0])
+    #print "Initial energy:",enlist[0]
+    # xsc is easy, just append each new energy to end of first file
+    bash.cp("%s/%s.xsc" % (name,proj), path)
+    xsc = open("%s/%s.xsc" % (path,proj), "a")    # append mode
+    
+    # .out file is tougher. start by copying lowest energy up to right before
+    # the 'CALCULATIONS COMPLETED SUCCESSFULLY' line:
+    out = open("%s/%s.out" % (path,proj), "w")
+    
+    out0 = open("%s/%s.out" % (name,proj), "r").readlines()
+    assert "CALCULATIONS COMPLETED SUCCESSFULLY" in out0[-1],\
+            "Empire crashed in %s"%name
+    #print "%s/%s.out" % (name,proj)
+    i = readPastOneEnergy(out0,0,end=True)   # line number for end
+    for j in range(i):
+        out.write( out0[j] )
+    
+    for e in enlist[1:]:
+        #print "next energy:",e
+        name = os.path.join(path,proj+"-"+e)
+        xscA = open("%s/%s.xsc" % (name,proj), "r").readlines()
+        xsc.write(xscA[3])
+        
+        outA = open("%s/%s.out" % (name,proj), "r").readlines()
+        assert "CALCULATIONS COMPLETED SUCCESSFULLY" in outA[-1],\
+                "Empire crashed in %s" % name
+        # read past the initial incident energy to where this energy starts:
+        i = readPastOneEnergy(outA,0)   # end of initial energy
+        i = readPastOneEnergy(outA,i+1)  # end of second energy
+        i2 = readPastOneEnergy(outA,i,end=True)
+        #print i,i2,len(outA)
+        for j in range(i,i2):
+            out.write( outA[j] )
+            
+        if e == enlist[-1]:
+            # done, write the footer
+            out.writelines(outA[i2:])
+    
+    xsc.close()
+    out.close()
+
+
+def reconstructXsec(inputFile):
+    """ 
+    reconstruct .xsc file only
+    More efficient for sensitivity calculation where we only need xsc
+    """    
+    path = getPath(inputFile)
+    proj = os.path.basename(inputFile).split('.')[0]
+    h,o,energies = parseInput(inputFile)
+    
+    # assume enlist is in order:
+    enlist = [e[0].strip() for e in energies]
+    
+    name = os.path.join(path,proj+"-"+enlist[0])
+    #print "Initial energy:",enlist[0]
+    # xsc is easy, just append each new energy to end of first file
+    bash.cp("%s/%s.xsc" % (name,proj), path)
+    xsc = open("%s/%s.xsc" % (path,proj), "a")    # append mode
+    
+    for e in enlist[1:]:
+        #print "next energy:",e
+        name = os.path.join(path,proj+"-"+e)
+        xscA = open("%s/%s.xsc" % (name,proj), "r").readlines()
+        xsc.write(xscA[3])
+        
+    xsc.close()
+
+
+def clean(inputFile):
+    """
+    remove all the 'single-energy' directories
+    """
+    from glob import glob
+    proj = inputFile.split('.')[0]
+    h,o,energies = parseInput(inputFile)
+    for e,opt in energies:
+        name = proj+"-"+e.strip()
+        try:
+            shutil.rmtree(name)
+        except OSError, e:
+            print e
+    for output in glob("*.log"):
+        os.remove(output)
+
+
+def getPath(inputFile):
+    """
+    this is a hack: os.abspath returns path that doesn't exist on compute nodes
+    must replace with real path
+    """
+    path = os.path.abspath(inputFile)
+    path = os.path.dirname(path).replace('/drives/4','/home2')
+    assert os.path.exists(path), "Check path (qsubEmpire.getPath function)!"
+    return path
+
+
+def process_args():
+    # see http://docs.python.org/lib/optparse-tutorial.html
+    from optparse import OptionParser
+    usage = "usage: <empireInput>.inp [options]"
+    parser = OptionParser(usage)
+    # bools are 'None' unless default set:
+    parser.set_defaults(verbose=False)
+    parser.add_option("-o", "--output", metavar="filename", 
+            help="send output to filename")
+    parser.add_option("-v", action="store_true", dest="verbose", 
+            help="enable verbose output")
+    parser.add_option("-c", "--clean", action="store_true", dest="clean", 
+            help="remove all subdirectories")
+    parser.add_option("-r", "--reconstruct", action="store_true",
+            dest="reconstruct", 
+            help="reconstruct full .xsc and .out files from all energies")
+    # help message (-h) is generated by OptionParser
+    
+    (options, args) = parser.parse_args()
+    
+    if options.verbose:
+        print ("options: " + repr(options))
+        print ("arguments: " + repr(args))
+    return (options, args)
+
+
+if __name__ == '__main__':
+    (opt, args) = process_args()
+    if len(args)==1:
+        if not os.path.exists(args[0]):
+            print "%s does not exist in local directory" % args[0]
+            sys.exit(1)
+    else:
+        print "required arg: empire input file"
+        sys.exit(1)
+    
+    if opt.clean:
+        clean(args[0])
+    elif opt.reconstruct:
+        reconstruct(args[0])
+    else:
+        # run!
+        runInput(args[0])
+        os.system('time ~/bin/monitorQueue 0 &')
+
---
+++ empy/qsubEmpire/sensitivity.py
@@ -0,0 +1,457 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+sensitivity.py
+Make full set of inputs from empire input and sensitivity input
+For each line in the sensitivity.inp file, two directories with inputs 
+are created
+
+Created by Caleb Mattoon on 2009-12-13.
+Copyright (c) 2009 __nndc.bnl.gov__. All rights reserved.
+"""
+
+import sys
+import os
+import shutil
+import time
+from empy import bash
+import qsubEmpire
+
+# which parameters can be varied in the EMPIRE INPUT?
+# allowed: can always be varied
+# restricted: variations only if defaults are provided
+# fisPars: vary the fission input file $proj-inp.fis
+allowed = ('ATILFI', 'ATILNO', 'CHMS', 'DEFDYN', 'DEFMSD', 'DEFNOR',
+        'DEFPAR', 'DEFSTA', 'FISBIN', 'FISBOU', 'FUSRED', 'GDIVP', 
+        'GDRST1', 'GDRST2', 'GDRWEI', 'GDRWP', 'GRANGN', 'GRANGP', 
+        'GTILNO', 'PCROSS', 'QFIS', 'RESNOR', 'SHELNO', 'TOTRED', 
+        'TUNE', 'TUNEFI', 'TUNEPE', 'UOMPAS', 'UOMPAV', 'UOMPRS', 
+        'UOMPRV', 'UOMPRW', 'UOMPVV', 'UOMPWS', 'UOMPWV')
+restricted = ('ALS', 'BETAV', 'BETCC', 'BFUS', 'BNDG', 'CRL', 'CSGDR1',
+        'CSGDR2', 'CSREAD', 'D1FRA', 'DEFGA', 'DEFGP', 'DEFGW', 'DFUS', 
+        'DV', 'EFIT', 'EGDR1', 'EGDR2', 'EX1', 'EX2', 'EXPUSH', 'FCC', 
+        'FCD', 'GAPN', 'GAPP', 'GCROA', 'GCROD', 'GCROE0', 'GCROT', 
+        'GCROUX', 'GDIV', 'GDRESH', 'GDRSPL', 'GDRWA1', 'GDRWA2', 'GGDR1', 
+        'GGDR2', 'HOMEGA', 'SHRD', 'SHRJ', 'SHRT', 'SIG', 'TEMP0', 'TORY',
+        'TRUNC', 'WIDEX')
+fisPars = ('VA','VB','VI','HA','HB','HI','DELTAF','GAMMA','ATLATF','VIBENH')
+
+# these global parameters don't need Z,A of isotope specified
+Globals = ('FUSRED','PCROSS','TOTRED','TUNEPE','GDIV','RESNOR')
+# not a complete list yet!
+
+
+def parseInput(proj):
+    """
+    there's also a 'parseInput' in the qsub script, 
+    here we modify that to keep all energy-dependent options as separate lines
+    """
+    fin = open(proj+".inp", "r")
+    header = [fin.next() for i in range(10)]
+    opts = []
+    while True:
+        line = fin.next()
+        if line[0] in ('#','!','*'):
+            continue    # ignore full-line comments
+        line = line.split('!')[0]   # eliminate end-of-line comments
+        line = line.rstrip() + "\n"
+        if line.startswith('GO'):
+            opts.append(line)
+            break
+        opts.append(line)
+    en = []
+    while True:
+        line = fin.next()
+        if line.startswith('-1'):
+            en.append(line)
+            break
+        en.append(line)
+    return header,opts,en
+
+
+def genNames(sensLine,proj):
+    """
+    from one line of sensitivity input, generate unique names for directory
+    for now, name is of form proj_PARAMETER_i1_i2_i3_i4(plus or minus), ie:
+    mn055_UOMPRS_0_1_0_0plus
+    """
+    tmp = sensLine.split('!')[0]
+    tmp = tmp.split()
+    while len(tmp) < 6:
+        tmp.append('0')
+    
+    name, val, i1, i2, i3, i4 = tmp[:]
+    str = proj+"_"+name + ("_%s"*4) % (i1,i2,i3,i4)
+    return (str+"plus",str+"minus")
+
+
+def init(proj):
+    """
+    Starting with 'good' empire input file 'proj.inp', 
+    modify parameters according to proj-inp.sen, 
+    generating two input files for each parameter varied
+    """
+    assert os.path.exists(proj+".inp") and os.path.exists(proj+"-inp.sen"),\
+            "required: %s.inp and %s-inp.sen in local directory" % (proj,proj)
+    
+    # read original input
+    header,options,energies = parseInput(proj)
+    # KALMAN and RANDOM options should be off:
+    for opt in options:
+        if opt.startswith('KALMAN') or opt.startswith('RANDOM'):
+            print ("Please remove KALMAN and RANDOM from input file!")
+            return 1
+    
+    # get a,z of target and projectile:
+    atarget, ztarget = (float(a) for a in header[1].split()[:2])
+    aproj, zproj = (float(a) for a in header[2].split()[:2])
+    
+    def readOption(inpString):
+        """
+        helper function: turn one line in the options to a list containing:
+        ['NAME', float(val), int(i1),int(i2),int(i3),int(i4)]
+        """
+        tmp = inpString.split('!')[0].split()
+        while len(tmp)<6:
+            tmp.extend('0')
+        return [tmp[0],float(tmp[1]),int(tmp[2]),int(tmp[3]),
+                int(tmp[4]),int(tmp[5])]
+    
+    # convert options to [string, float, int*4] for easier comparison:
+    optvals = []
+    for opt in options:
+        optvals.append( readOption(opt) )
+    #print optvals
+    
+    # create dir for original input.
+    # Also copy -inp.fis, .lev and -lev.col if available
+    os.mkdir(proj+"_orig")
+    shutil.copy(proj+".inp",proj+"_orig/")
+    bash.cp(proj+"-inp.fis",proj+"_orig/",False)
+    bash.cp(proj+".lev",proj+"_orig/",False)
+    bash.cp(proj+"-lev.col",proj+"_orig/",False)
+
+
+    # open sensitivity input:
+    sens = open(proj+"-inp.sen", "r") # sensitivity input
+    
+    # main loop: vary each parameter in the sensitivity input:
+    for line in sens:
+        if line.strip()=='' or line[0] in ('!','#','*'):
+            continue
+        
+        tmp = readOption(line)
+        name, val, i1, i2, i3, i4 = tmp[:]
+        # some parameters are isotope-specific (optical model, for example)
+        # in this case, we get Z,A of isotope from i1, i2:
+        if not name in Globals:
+            i2 = atarget-i1-i2 + aproj-zproj
+            i1 = ztarget-i1
+        
+        if name in fisPars:
+            # these are a special case, require modifying the fission input
+            # I've separated that code into another module
+            import parseFission
+            parseFission.parseFission(proj, line, (name,val,i1,i2,i3,i4))
+            continue
+        
+        if not (name in allowed or name in restricted):
+            print "%s can't be varied. Skipping" % name
+            continue
+        
+        def isMatch(opt):
+            """ helper: does this line in the sensitivity input have
+            a match in the empire input? """
+            #if name in Globals:  # for global pars, only need to match name
+            #    return opt[0]==name
+            return [opt[a] for a in (0,2,3,4,5)] == [name,i1,i2,i3,i4]
+        
+        # now try to find equivalent option in the empire input
+        # if it shows up several times, only last occurance matters
+        foundOpt = False
+        optIdx = len(optvals)-1  # what line is option on?
+        for opt in optvals[::-1]:
+            if isMatch(opt):
+                # found a match between sensitivity and basic inputs
+                print "found match", name
+                foundOpt = True
+                break
+            optIdx -= 1
+        
+        # write new input files with variations:
+        template = "%-6s%10.3f%5i%5i%5i%5i\n"
+        
+        # make unique names for 'val+sigma' and 'val-sigma' directories:
+        nameP, nameM = genNames(line,proj)
+        
+        if not foundOpt and name in restricted:
+            print "no default values for %s. Please supply in input"%name
+            continue    # go to next line of sensitivity input
+        else:
+            os.mkdir( nameP )
+            os.mkdir( nameM )
+            
+            # copy files to newly-created dir if available
+            # suppress error messages if not:
+            for dir in (nameP,nameM):
+                bash.cp(proj+"-inp.fis",dir,False)
+                bash.cp(proj+".lev",dir,False)
+                bash.cp(proj+"-lev.col",dir,False)
+                bash.cp(proj+"-omp.dir",dir,False)
+                bash.cp(proj+"-omp.ripl",dir,False)
+            
+            plus = open( nameP+"/%s.inp" % proj, "w")
+            minus = open( nameM+"/%s.inp" % proj, "w")
+            plus.writelines(header)
+            minus.writelines(header)
+            
+            def writepm(valplus, valminus):
+                # write lines to 'plus' and 'minus' files 
+                # (implicit convert to str)
+                plus.write(template % (name,valplus,i1,i2,i3,i4) )
+                minus.write(template % (name,valminus,i1,i2,i3,i4) )
+            
+            if foundOpt:    # vary option where it appears:
+                plus.writelines(options[:optIdx])
+                minus.writelines(options[:optIdx])
+                orig = optvals[optIdx][1]   #value in orig. input
+                writepm( orig*(1+val), orig*(1-val) )
+                plus.writelines(options[optIdx+1:])
+                minus.writelines(options[optIdx+1:])
+            else:       # add option right before 'GO':
+                plus.writelines(options[:-1])
+                minus.writelines(options[:-1])
+                if name.startswith('UOMP'):
+                    writepm( -(1.0+val),-(1.0-val) )
+                else:
+                    writepm( 1.0+val, 1.0-val )
+                plus.writelines(options[-1])
+                minus.writelines(options[-1])
+        
+        # after GO, write energies:
+        for en in energies:
+            if en[0]=='$':
+                tmp = readOption(en[1:]) 
+                if isMatch(tmp):
+                    # this energy-dependant option should be varied:
+                    print "match in e-dep opts: %s" % name
+                    orig = tmp[1]
+                    plus.write('$'+template %
+                        (name,orig*(1+val),i1,i2,i3,i4) )
+                    minus.write('$'+template %
+                        (name,orig*(1-val),i1,i2,i3,i4) )
+                else:
+                    # energy-dependant option, but not currently being varied
+                    plus.write(en)
+                    minus.write(en)
+            else:
+                # single energy
+                plus.write(en)
+                minus.write(en)
+        plus.close()
+        minus.close()
+
+
+def copyTLs(proj):
+    """
+    after running original input, pass TLs to other parameters
+    should improve performance
+    """
+    sens = open(proj+"-inp.sen", "r")
+    for line in sens:
+        if line.strip()=='' or line[0] in ('!','#','*'):
+            continue
+        if line.startswith("UOM"):
+            # don't copy tls when we vary the optical model parameters
+            continue
+    
+        tmp = line.split('!')[0]
+        tmp = tmp.split()
+        while len(tmp) < 6:
+            tmp.append('0')
+        
+        name, val, i1, i2, i3, i4 = tmp[:]
+        if not (name in allowed or name in restricted
+                or name in fisPars):
+            continue
+        
+        nameP, nameM = genNames(line,proj)
+        
+        # ... won't work as desired without changes to qsubEmpire:
+        # right now, each independant energy is only created when runInput()
+        # is called by qsubEmpire. Need to copy these TLs into those subdirs
+
+
+def run(proj):
+    """
+    use init to set up the project, then run!
+    """
+    print "Starting original input"
+    # 'clean=True': delete everything but .xsc file after running
+    qsubEmpire.runInput("%s_orig/%s.inp" % (proj,proj), clean=False)
+    
+    # start watching queue, report when all jobs finish:
+    os.system('time ~/bin/monitorQueue 0 &')
+    
+    # open sensitivity input:
+    sens = open(proj+"-inp.sen", "r") # sensitivity input
+    
+    for line in sens:
+        if line.strip()=='' or line[0] in ('!','#','*'):
+            continue
+        
+        tmp = line.split('!')[0]
+        tmp = tmp.split()
+        while len(tmp) < 6:
+            tmp.append('0')
+        
+        name, val, i1, i2, i3, i4 = tmp[:]
+        if not (name in allowed or name in restricted
+                or name in fisPars):
+            continue
+        
+        nameP, nameM = genNames(line,proj)
+        # pause between submitting each new file:
+        time.sleep(2)
+        print "Starting", nameP
+        qsubEmpire.runInput(nameP+"/%s.inp" % proj, clean=True)
+        time.sleep(2)
+        print "Starting", nameM
+        qsubEmpire.runInput(nameM+"/%s.inp" % proj, clean=True)
+    
+    # after submitting all inputs (better to start monitoring earlier)
+    #os.system('time ~/bin/monitorQueue 0 &')
+
+
+def analyze(proj):
+    """
+    generate sensitivity matrix after run() finishes
+    """
+    import numpy
+    # file for writing matrix:
+    senfile = open("%s-mat.sen" % proj, "w")
+    
+    def getCrossSection(filename):
+        """
+        return numpy array with x-sections from empire input 'filename'
+        """
+        qsubEmpire.reconstructXsec(filename)
+        xsecfile = filename.replace(".inp",".xsc")
+        xsecfile = open(xsecfile,"r")
+        arr = []
+        for l in xsecfile.readlines()[2:]:
+            # read all x-sections into array
+            vals = [float(a) for a in l.split()]
+            arr.append(vals)
+        xsecfile.close()
+        return numpy.array( arr )
+    
+    # start with original input:
+    xs0 = getCrossSection("%s_orig/%s.inp" % (proj,proj))
+    
+    # grab two 'header' lines from original input:
+    f = open("%s_orig/%s.xsc" % (proj,proj),"r")
+    header = (f.next(),f.next())
+    f.close()
+    
+    # open sensitivity input:
+    sens = open(proj+"-inp.sen", "r") # sensitivity input
+    
+    for line in sens:
+        if line.strip()=='' or line[0] in ('!','#','*'):
+            continue
+        
+        tmp = line.split('!')[0]
+        tmp = tmp.split()
+        while len(tmp) < 6:
+            tmp.append('0')
+        
+        name, val, i1, i2, i3, i4 = tmp[:]
+        if not (name in allowed or name in restricted
+                or name in fisPars):
+            print "Parameter %s shouldn't be varied."%name\
+                    + " Will not include in sensitivity output."
+            continue
+        val = float(val)
+        i1,i2,i3,i4 = [int(a) for a in (i1,i2,i3,i4)]
+        
+        nameP, nameM = genNames(line,proj)
+        # reconstruct cross sections for val+sigma and val-sigma:
+        xsplus = getCrossSection(nameP+"/%s.inp" % proj)
+        xsminus = getCrossSection(nameM+"/%s.inp" % proj)
+        
+        # make a modified version of sens. matrix, KALMAN-specific
+        smat = (xsplus[:,1:]-xsminus[:,1:]) / xs0[:,1:]
+        # this produces 'nan' wherever xs0==0. Replace 'nan' with 0
+        smat[xs0[:,1:]==0] = 0.0
+        # also truncate sensitivities below 10**-5
+        smat[abs(smat)<1e-5] = 0.0
+        
+        # 'sens' contains only sensitivities. Also create 'en' with energies:
+        en = xs0[:,0]
+        
+        # write matrix to file
+        senfile.write(header[0])
+        str = "# Parameter: %-6s  %3i%3i%3i%3i  variation: +-%5.3f"+\
+                "     Sensitivity matrix\n"
+        senfile.write(str % (name,i1,i2,i3,i4,val))
+        senfile.write(header[1].rstrip()+' \n')
+        for i in range(len(smat)):
+            if en[i]<0.1:
+                str = "%-10.4E"%en[i]
+            else:
+                str = "%#-10.5G"%en[i]
+            #str = str + smat.shape[1]*"%12.4E"%tuple(smat[i]) + "\n"
+            str = str+smat.shape[1]*"%12.4E"%tuple(smat[i])+"\n"
+            # or write one extra 0.0 to match previous style:
+            #str = str+smat.shape[1]*"%12.4E"%tuple(smat[i])+"%12.4E"%0+"\n"
+            senfile.write(str)
+        senfile.write("\n")
+    
+    sens.close()
+    senfile.close()
+
+
+def process_args():
+    # see http://docs.python.org/lib/optparse-tutorial.html
+    from optparse import OptionParser
+    usage = """\
+usage: <empireInput>.inp [options]
+no options: setup and run all parameters"""
+    parser = OptionParser(usage)
+    # bools are 'None' unless default set:
+    parser.set_defaults(verbose=False)
+    parser.add_option("-i", action="store_true", dest="init", 
+            help="setup files for running sensitivity analysis")
+    parser.add_option("-r", action="store_true", dest="run",
+            help="run (after initializing manually)")
+    parser.add_option("-a", "--analyze", action="store_true", dest="analyze", 
+            help="after running, generate sensitivity matrix")
+    parser.add_option("-c", "--clean", action="store_true", dest="clean", 
+            help="remove all subdirectories")
+    # help message (-h) is generated by OptionParser
+    
+    (options, args) = parser.parse_args()
+    return (options, args)
+
+
+def main():
+    opts, args = process_args()
+    inputFile = args[0]
+    proj = inputFile.split('.')[0]
+    if opts.clean:
+        os.system('rm -r %s_*' % proj)
+    elif opts.init:
+        init(proj)
+    elif opts.run:
+        run(proj)
+    elif opts.analyze:
+        analyze(proj)
+    else: # if no options, setup and run the analysis
+        init(proj)
+        run(proj)
+
+
+if __name__ == '__main__':
+    main()
+
