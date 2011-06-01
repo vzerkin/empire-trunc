4f66ead0f67005981590d0524f64e6793a565751
Roberto Capote <rcapotenoy@yahoo.com>
2011-06-01 21:13:32 +0200

    svn:2054 New EMPIRE-3.1 setup

--- setup-empire-3.1.sh
+++ setup-empire-3.1.sh
@@ -1,5 +1,6 @@
 #! /bin/sh -
-#script to install and compile EMPIRE-3.2 package
+#script to install and compile EMPIRE-3.1 package
+#
 #directory in which EMPIRE is to be installed
 instdir=$HOME
 #directory with EMPIRE distribution
@@ -14,25 +15,24 @@ echo '    E      M    M  P      I  R  R   E              3       '
 echo '    EEEEE  M    M  P      I  R   R  EEEEE      33333       '
 echo '                                                           '
 echo '        A R C O L E (release '$empver'), June 2011         '
-echo '_______________________________________________________________________________________________________________'
-echo '                                                           '
-echo ' Please refer to:                                          '
-echo ' 1)M.Herman, R.Capote, B.Carlson, P.Oblozinsky, M.Sin, A.Trkov, H.Wienke, and V.Zerkin                         '
-echo '  "EMPIRE: Nuclear Reaction Model Code System for data evaluation", Nuclear Data Sheets 108 (2007) 2655-2715   '
-echo '                                                                                                               '
-echo ' 2)R. Capote, M. Herman, P. Oblozinsky, P.G. Young, S. Goriely, T. Belgya, A.V. Ignatyuk, A.J. Koning,         '
-echo '   S. Hilaire, V.A. Plujko, M. Avrigeanu, Zhigang Ge, Yinlu Han, S. Kailas, J. Kopecky, V.M. Maslov,           '
-echo '   G. Reffo, M. Sin, E.Sh. Soukhovitskii, and P. Talou                                                         '
-echo '  "RIPL - Reference Input Parameter Library for Calculation of Nuclear Reactions and Nuclear Data Evaluations",'
-echo '   Nuclear Data Sheets 110 (2009) 3107-3214                '
-echo '   RIPL available online at http://www-nds.iaea.org/RIPL-3/'
-echo '_______________________________________________________________________________________________________________'
+echo '_______________________________________________________________________________________'
+echo '                                                                                       '
+echo ' Please refer to:                                                                      '
+echo ' 1)M.Herman, R.Capote, B.Carlson, P.Oblozinsky, M.Sin, A.Trkov, H.Wienke, and V.Zerkin '
+echo '  "EMPIRE: Nuclear Reaction Model Code System for data evaluation",                    '
+echo '   Nuclear Data Sheets 108 (2007) 2655-2715                                            '
+echo '                                                                                       '
+echo ' 2)R.Capote, M.Herman, P.Oblozinsky, P.G.Young, S.Goriely, T.Belgya, A.V.Ignatyuk,     '
+echo '   A.J.Koning, S.Hilaire, V.A.Plujko, M.Avrigeanu, Zhigang Ge, Yinlu Han, S.Kailas,    '
+echo '   J. Kopecky, V.M. Maslov, G.Reffo, M.Sin, E.Sh.Soukhovitskii, and P.Talou,           '
+echo '  "RIPL - Reference Input Parameter Library for Calculation of Nuclear Reactions and   '
+echo '   Nuclear Data Evaluations", Nuclear Data Sheets 110 (2009) 3107-3214                 '
+echo '   RIPL available online at http://www-nds.iaea.org/RIPL-3/                            '
+echo '_______________________________________________________________________________________'
 echo ' '
-echo 'This is the EMPIRE-3 (release '$empver') installation script.'
-echo 'It will lead you through the set-up procedure and eventually '
-echo 'compile the whole the package. '
+echo 'This is the EMPIRE-3 (release '$empver') installation script. It will lead you through '
+echo 'the set-up procedure and eventually compile the whole the package. '
 echo ' '
-echo 'Source directory with EMPIRE-3 distribution ['$sourcedir']:'
 echo '  Press ENTER to start the EMPIRE setup, CTRL-C to cancel'
 read dir
 #if [ ! "$dir" = "" ]; then
@@ -44,8 +44,8 @@ if [ -f $sourcedir/EMPIRE-$empver.tgz ]; then
    echo ' '
 else
    echo ' '
-   echo 'EMPIRE-'$empver'.tgz not found in '$sourcedir', restart setup script '
-   echo 'and specify the correct path to the EMPIRE-'$empver'.tgz file'
+   echo 'EMPIRE-'$empver'.tgz not found in '$sourcedir', please restart the setup script in '
+   echo ' the directory containing the EMPIRE-'$empver'.tgz file'
    exit
 fi
 echo 'Specify a directory in which EMPIRE-3 should be installed.'
@@ -56,6 +56,7 @@ instdir=$HOME/empire
 if [ ! "$dir" = "" ]; then
    instdir=$HOME/$dir
 fi
+echo ' '
 echo 'EMPIRE will be installed into the directory ['$instdir']:'
 echo ' '
 echo '  Press any key to confirm this setup, CTRL-C to cancel '
@@ -66,9 +67,8 @@ export EMPIREDIR
 cd $instdir
 echo ' '
 echo 'Exploding EMPIRE-3 release $empver source'
-#  gunzip $sourcedir/EMPIRE-$empver.tgz
-#  tar xvf $sourcedir/EMPIRE-$empver.tar
 tar xzvf $sourcedir/EMPIRE-$empver.tgz
+echo ' '
 echo 'EMPIRE-3 system decompressed in the directory '$instdir
 echo ' '
 echo '  Press ENTER to continue the setup, CTRL-C to cancel'
@@ -80,17 +80,18 @@ if [ -f $sourcedir/level-densities-hfb.tgz ]; then
    echo ' '
 else
    echo ' '
-   echo 'level-densities-hfb.tgz not found in '$sourcedir', go to the EMPIRE webpage,'
-   echo 'download it and restart the script'
+   echo 'level-densities-hfb.tgz not found in '$sourcedir', please restart the '
+   echo ' setup script in the directory containing the EMPIRE-'$empver'.tgz file'
+   echo ' '
    exit
 fi
 echo 'Exploding microscopic HFB level densities in the directory '$instdir'/RIPL-2/densities/total/level-densities-hfb'
 cd $instdir/RIPL-2/densities/total/level-densities-hfb
-#  gunzip $sourcedir/level-densities-hfb.tgz
-#  tar xvf $sourcedir/level-densities-hfb.tgz
 tar xzvf $sourcedir/level-densities-hfb.tgz
+echo ' '
 echo 'RIPL-3 HFB level densities decompressed in directory '$instdir/RIPL-2/densities/total/level-densities-hfb
 cd $instdir
+echo ' '
 echo '  Press ENTER to continue the setup, CTRL-C to cancel'
 read dir
 echo ' '
@@ -100,14 +101,13 @@ if [ -f $sourcedir/C4-IAEA-Jan2011.tgz ]; then
    echo ' '
 else
    echo ' '
-   echo 'C4-IAEA-Jan2011.tgz not found in '$sourcedir', go to the EMPIRE webpage,'
-   echo 'download it and restart the script'
+   echo 'C4-IAEA-Jan2011.tgz not found in '$sourcedir', please restart the '
+   echo ' setup script in the directory containing the EMPIRE-'$empver'.tgz file'
    exit
 fi
 echo 'Exploding EXFOR(C4) experimental data retrieved from IAEA/NDS'
-#  gunzip $sourcedir/C4-IAEA-Jan2011.tgz
-#  tar xvf $sourcedir/C4-IAEA-Jan2011.tgz
 tar xzvf $sourcedir/C4-IAEA-Jan2011.tgz
+echo ' '
 echo 'EXFOR(C4) data decompressed in the directory '$instdir
 echo ' '
 echo '  Press ENTER to continue the setup, CTRL-C to cancel'
@@ -115,26 +115,20 @@ read dir
 echo ' '
 # Installing Tcl/Tk packages
 echo ' '
-echo 'We will need Tcl/Tk and itcl packages to run EMPIRE GUI. '
-echo 'Although your system might have these packages installed  '
-echo 'it often happens that they are not compatible with EMPIRE. '
-echo 'To be on a safe side We would install industry standard  '
-echo 'ActiveTcl package that runs on most Linuxes.'
+echo 'We will need Tcl/Tk and Itcl packages to run EMPIRE GUI. Although your system    '
+echo 'might have these packages installed it often happens that they are not compatible'
+echo 'with EMPIRE. To be on a safe side we would install industry standard ActiveTcl   '
+echo 'package that runs on most Linuxes.'
 echo ' '
-echo 'We strongly recommend to install this package. It will be '
-echo 'installed locally in the empire directory and will not mess up '
-echo 'with your native Tcl/Tk installation. To make it active you will'
-echo 'have to add a line to your shell initialization file as indicated '
-echo 'by the ActiveTcl installation script. To return to your native Tcl '
-echo 'it is enough to remove this line, then logoff and login.'
-echo 'If you decide to install ActiveTcl say yes below and follow the'
-echo 'instructions (you may choose '$instdir/ActiveTcl' as an'
-echo 'installation directory) '
+echo 'The ActiveTcl graphical setup will request the Tcl installation directory:       '
+echo 'Please specify installation directory (default:/usr/local/username               '
 echo ' '
-echo 'DO NOT FORGET TO ADD THE LINE AS ADVISED, THEN CLOSE THE CONSOLE '
-echo ' AND REOPEN IT! '
+echo 'You should overwrite the default selection by writing '$instdir/ActiveTcl        '
 echo ' '
-echo 'Do you want to install ActiveTcl package y/n [y]:'
+echo 'Please note that this package will be installed locally in the empire directory  '
+echo 'and will not mess up with your native Tcl/Tk installation (if any).              '
+echo ' '
+echo 'Do you want to proceed with installation of the ActiveTcl package y/n [y]:'
 read yesno
 if [ ! "$yesno" = "" ]; then
         activetcl=$yesno
@@ -190,6 +184,12 @@ cd $HOME
 # Modifying .bashrc
 #
 echo 'Adding EMPIREDIR to .bashrc file'
+#
+newpath=./:$instdir/scripts:$instdir/ActiveTcl/bin:$PATH
+if [ "$activetcl" = "n" ]; then
+newpath=./:$instdir/scripts:$PATH
+fi
+#
 cat >>.bashrc <<EOF
 # ___________________________________________________________________
 # Lines added by EMPIRE-3.1 setup
@@ -199,64 +199,65 @@ cat >>.bashrc <<EOF
 # EMPIREDIR is pointing to the active EMPIRE installation
 #
 EMPIREDIR=$instdir
-echo 'EMPIRE directory changed to '$instdir 
-PATH=./:$instdir/scripts:$HOME/ActiveTcl/bin:$PATH
+echo '
+echo 'EMPIRE 3.1 directory set to '$instdir 
+echo '
+PATH=$newpath
 export EMPIREDIR PATH 
 EOF
+source .bashrc 
 EMPIREDIR=$instdir
-PATH=./:$instdir/scripts:$HOME/ActiveTcl/bin:$PATH
+PATH=$newpath
 export EMPIREDIR PATH 
 echo 'EMPIREDIR environment variable set to '$EMPIREDIR    
 echo ' '
 echo '  Press ENTER to continue the setup, CTRL-C to cancel'
 read dir
 echo ' '
-echo '________________________________________________________________'
+echo '______________________________________________________________________'
 echo ' '
-echo ' Please do not forget to extend your PATH variables to          '
-echo ' get access to ActiveTcl and empire binaries (zvview,empire3)   '
+echo ' We extended your PATH variables to get access to ActiveTcl and       '
+echo ' empire binaries for a shell sh enviroment (.bashrc)                  '
 echo ' '
-echo ' For a shell sh enviroment (.bashrc), it is done in this script '
+echo ' For a sh or similar perform                                          '
+echo '     EMPIREDIR="'$instdir'"                                           '
+echo '     PATH="'$instdir/ActiveTcl/bin:./:$instdir/scripts':$PATH"        '
+echo '     export EMPIREDIR PATH                                            '
 echo ' '
-echo ' For a sh or similar perform                                    '
-echo '     EMPIREDIR="'$instdir'"'
-echo '     PATH="'$HOME/ActiveTcl/bin:./:$instdir/scripts':$PATH"'
-echo '     export EMPIREDIR PATH                                      '
+echo ' For a csh or compatible perform                                      '
+echo '     setenv EMPIREDIR "'$instdir'"                                    '
+echo '     setenv PATH "'$instdir/ActiveTcl/bin:./:$instdir/scripts':$PATH" '
+echo '     export EMPIREDIR PATH                                            '
 echo ' '
-echo ' For a csh or compatible perform'
-echo '     setenv EMPIREDIR "'$instdir'"'
-echo '     setenv PATH "'$HOME/ActiveTcl/bin:./:$instdir/scripts':$PATH" '
-echo '     export EMPIREDIR PATH '
+echo ' Some shells (bash for example) allow                                 '
+echo '     export PATH="'$instdir/ActiveTcl/bin:./:$instdir/scripts':$PATH" '
 echo ' '
-echo ' Some shells (bash for example) allow                      '
-echo '     export PATH="'$HOME/ActiveTcl/bin:./:$instdir/scripts':$PATH"   '
-echo ' Once you modify your path variables close your console and reopen it'
-echo '_____________________________________________________________________'
-read dir
-echo '                                                           '
-echo ' Additional information and previous versions available at '
-echo ' EMPIRE homepages:                                         '
-echo ' . International Atomic Energy Agency, Vienna, Austria @ http://www-nds.iaea.org/empire/'                      
-echo ' . Brookhaven National Laboratory, NNDC, New York, USA @ http://www.nndc.bnl.gov/nndcscr/model-codes/empire-ii/'
+echo ' Once you modify your path variables close your console and reopen it '
+echo '______________________________________________________________________'
+echo '                                                                      '
+echo ' Additional information and previous versions available at            '
+echo ' EMPIRE homepages:                                                    '
+echo ' International Atomic Energy Agency, Vienna, Austria                  '
+echo '       @ http://www-nds.iaea.org/empire/                              '                      
+echo ' Brookhaven National Laboratory, NNDC, New York, USA                  '             
+echo '       @ http://www.nndc.bnl.gov/empire-3.1/                          '
 echo ' '
-echo '_____________________________________________________________________'
-echo 'Assuming your working directory is "work"'
-echo 'change to the '$instdir/work' directory and type:'
+echo '______________________________________________________________________'
 echo ' '
-echo ' ../scripts/Xrun.tcl &'
-echo '     or '
-echo ' empire3 '
+echo 'Assuming your working directory is '$instdir/work', you should type:  '
+echo ' '
+echo ' cd '$instdir/work
+echo ' $instdir/scripts/Xrun.tcl &'
 echo ' '
 echo ' to start EMPIRE GUI.'
 echo ' '
 echo ' Bons calculos !'
-echo '                   '
 echo ' The EMPIRE team, Sao Jose do Campos, Brazil, May 2011.'
 echo ' '
 echo '  Press ENTER to launch EMPIRE GUI, CTRL-C to cancel'
 read dir
 echo ' '
 cd $instdir/work
-../scripts/empire3 &
+$instdir/scripts/Xrun.tcl &
 exit
 
