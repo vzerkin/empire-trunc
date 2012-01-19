523e344e1f74c28a035d0508a2c4b3a14820ad9c
Roberto Capote <rcapotenoy@yahoo.com>
2012-01-19 21:09:53 +0100

    svn:2253 Current version by BC introduces some changes that affects the agreement between calculated spectra and cross sections. Further checks of spectra calculated with PCROSS and/ or HF methods are needed to clarify this issue.
    The last version not affected by current changes is the version 2228.

--- source/print.f
+++ source/print.f
@@ -1,7 +1,17 @@
-Ccc   * $Rev: 2244 $
+Ccc   * $Rev: 2253 $
 Ccc   * $Author: rcapote $
-Ccc   * $Date: 2012-01-19 04:02:04 +0100 (Do, 19 Jän 2012) $
+Ccc   * $Date: 2012-01-19 21:09:53 +0100 (Do, 19 Jän 2012) $
 
+C
+C     Current version introduces soem changes that affects the agreement 
+C     between calculated spectra and cross sections. Further checks of 
+C     spectra calculated with PCROSS and.or HF contributions only are needed
+C     to clarify this issue.
+C     The last version not affected by current changes is the version 2228 
+C     (jan 2012)
+C
+C     RC, Jan. 19, 2012 
+C
 C
       SUBROUTINE Print_Total(Nejc)
 Ccc
@@ -44,30 +54,52 @@ C
       DATA hstar, haha/'*', ' '/
 
       csemax = 0.
+
       kmax = 1
+
       DO i = 1, NDECSE
+
          IF (CSEt(i,Nejc).GT.0.d0) kmax = i
+
          csemax = DMAX1(CSEt(i,Nejc),csemax)
+
       ENDDO
+
 C
+
 C     Stringest test to avoid plotting problems.
+
 C     Cross sections smaller than 1.d-4 mb are not relevant at all.  
+
 C
+
       IF (csemax.LE.1.d-5) return
 
+
       kmax = kmax + 1
+
       kmax = MIN0(kmax,NDECSE)
 
+
       totspec = 0.d0
+
       DO i = 1, kmax
+
         totspec  = totspec  + CSEt(i,Nejc)
+
       ENDDO
+
       totspec = totspec - 
+
      &          0.5d0*(CSEt(1,Nejc) + CSEt(kmax,Nejc))
+
       totspec = totspec*DE     
+
       IF (totspec.LE.1.d-4) RETURN
 
+
       ia = AEJc(Nejc)
+
       IF (Nejc.EQ.0) THEN
          WRITE (8,99005)
 99005    FORMAT (1X,/,1X,54('*'),1X,'gamma spectrum  ',54('*'),//)
@@ -143,6 +175,7 @@ C
       totspec = totspec*DE
       WRITE (8,99045)
       WRITE (8,'(1x,''    Integrated spectrum   '',G12.5,''  mb'')')
+
      &          totspec 
 99045 FORMAT (24X,93('-'))
       END
@@ -159,6 +192,7 @@ Ccc   *                                                                  *
 Ccc   * input:NNUC-decaying nucleus index                                *
 Ccc   *       NEJC-ejectile index                                        *
 Ccc   *       Iflag=1 for integral of inclusive spectra (special case)   *
+
 Ccc   *               Usually Iflag=0 for normal exclusive spectra       *
 Ccc   * output:none                                                      *
 Ccc   *                                                                  *
@@ -244,7 +278,7 @@ C      kmax = MIN0(kmax,NDECSE)
          ENDIF
       ENDIF
 
-	recorp = 1.d0
+      recorp = 1.d0
       if(Nejc.gt.0) recorp = 1.d0 + EJMass(Nejc)/AMAss(Nnuc)
       
       n = IFIX(SNGL(LOG10(csemax*recorp) + 1.))
@@ -339,15 +373,24 @@ C
 
       IF (csemax.LE.1.d-5) return
       kmax = kmax + 1
+
       kmax = MIN0(kmax,NDECSE)
 
+
       totspec = 0.0
+
       DO i = 1, kmax
+
          totspec  = totspec  + CSE(i,Nejc,Nnuc)
+
       ENDDO
+
       totspec = totspec - 0.5*(CSE(1,Nejc,Nnuc) + CSE(kmax,Nejc,Nnuc))
+
       totspec = totspec*DE
+
       IF (totspec.LE.1.d-4) RETURN
+
 
       if(SYMb(Nnuc)(2:2).eq.' ') then
         write(caz,'(A3,I2.2,A1,A1,I3.3,A1,A1,A4)')
@@ -365,13 +408,17 @@ C
      & '(a5, i2,1h-,A2,1h-,I3,3h(x, ,a1, 2h): ,F8.2, 2Hmb)')
      & 'tit: ',int(Z(Nnuc)),SYMb(Nnuc),int(A(Nnuc)),part(Nejc),totspec
 
-	recorp = 1.d0
+      recorp = 1.d0
+
       if(Nejc.gt.0) recorp = 1.d0 + EJMass(Nejc)/AMAss(Nnuc)
+
+
 
       CALL OPEN_ZVV(36,'SP_'//part(Nejc),title)
       DO i = 1, kmax
       IF(CSE(i,Nejc,Nnuc).LE.0.d0) CYCLE
          WRITE (36,'(1X,E12.6,3X,E12.6)') 
+
      &     FLOAT(i - 1)*DE*1.D6/recorp, 
      &       CSE(i,Nejc,Nnuc)*recorp*1.d-3 ! Energy, Spectra in b/MeV
       ENDDO
@@ -435,8 +482,11 @@ C
       totspec = totspec*DE
       IF (totspec.LE.1.d-4) RETURN
 
+
       write(caz,'(A3,A1,A4)') 'sp_',part(Nejc),'.zvd'
+
       OPEN(36,file=caz,status='unknown')
+
       write(title,'(a13,3h(x, ,a1, 2h): ,F8.2, 2Hmb)')
      & 'tit: Total Emission Spectra ',part(Nejc),totspec
 
@@ -450,6 +500,5 @@ C
       CLOSE(36)
       RETURN
       END
-
 
 
