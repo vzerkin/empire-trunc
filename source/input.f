3c9a2b117955a065e4d1a7d9ae76fef71e101454
Roberto Capote <rcapotenoy@yahoo.com>
2012-01-28 09:34:54 +0100

    svn:2355 read_nubar routine separated from input.f. Problems arised in WINDOWS compilation as SH warned. Perhaps a better solution will be to transform this routine into script to be called before running calculations for fissioning nuclei. (AFTER release)

--- source/input.f
+++ source/input.f
@@ -1,6 +1,6 @@
-Ccc   * $Rev: 2345 $
-Ccc   * $Author: bcarlson $
-Ccc   * $Date: 2012-01-27 21:22:51 +0100 (Fr, 27 Jän 2012) $
+Ccc   * $Rev: 2355 $
+Ccc   * $Author: rcapote $
+Ccc   * $Date: 2012-01-28 09:34:54 +0100 (Sa, 28 Jän 2012) $
       SUBROUTINE INPUT
 Ccc
 Ccc   ********************************************************************
@@ -275,8 +275,9 @@ C        CHECKING and READING the NUBAR-EVAL.ENDF
 C
 C        READING OF THE ENDF MF=1, MT=456 prompt nubar
 C        and initialization of the ENIu_eval(Einc) ,VNIu_eval(Einc) global arrays 
-         IF(NUBarread) CALL READNUBAR(nubar_filename,
-     &len_trim(nubar_filename))
+
+         IF(NUBarread) 
+     &   CALL READNUBAR(nubar_filename,len_trim(nubar_filename))
 C      
          NPRIm_g = 0       ! No primary gammas (default)        
          FISspe = 0
@@ -9887,58 +9888,3 @@ Ccc
       RETURN
       END
 
-
-      SUBROUTINE READNUBAR(infile,nin)
-
-      use endf_io
-
-      INCLUDE 'dimension.h'
-      INCLUDE 'global.h'
-
-      integer*4, intent(in) :: nin
-      character*200, intent(in) :: infile
-
-      integer*4 np
-
-      type (endf_mat) nubar_mat
-      type (mf_1), pointer :: mf1
-      
-      call clear_mat(nubar_mat)
-      allocate(nubar_mat%mf1)
-
-      call find_mf(infile(1:nin),nubar_mat)
-
-      mf1 => nubar_mat%mf1
-      do while(associated(mf1))
-         if(mf1%mt .eq. 456) exit
-	 mf1 => mf1%next
-      end do
-      
-      if(.not.(associated(mf1))) then
-          write(8,*) ' MT456 not found in ',infile(1:nin)
-          stop ' Aborted in readNubar'
-      endif 
-      
-      if(mf1%mt456%lnu .eq. 2) then
-        np = mf1%mt456%tb%np
-        eniu_eval = 0.0d0
-        vniu_eval = 0.0d0
-	if(np .gt. ndecse) then
-	    write(8,*) ' WARNING: # nubar energies > storage array'
-	    np = ndecse
-	endif
-c	Assigning energies to eniu_eval and converting to MeV
-        eniu_eval(1:np) = mf1%mt456%tb%dat%x/1.d6
-c	Assigning nubars to vniu_eval
-        vniu_eval(1:np) = mf1%mt456%tb%dat%y
-	num_niu = np
-       else
-        write(8,*) 'ERROR Reading nubar!'
-	write(8,*) 'Only option lnu=2 in MT=456 has been implemented!'
-	stop ' Abort in readNubar'
-      endif
-      
-      call del_mf(mf1)
-
-      return      
-      END
---
+++ source/read_nubar.f
@@ -0,0 +1,55 @@
+
+      SUBROUTINE READNUBAR(infile,nin)
+
+      use endf_io
+
+      INCLUDE 'dimension.h'
+      INCLUDE 'global.h'
+
+      integer*4, intent(in) :: nin
+      character*200, intent(in) :: infile
+
+      integer*4 np
+
+      type (endf_mat) nubar_mat
+      type (mf_1), pointer :: mf1
+      
+      call clear_mat(nubar_mat)
+      allocate(nubar_mat%mf1)
+
+      call find_mf(infile(1:nin),nubar_mat)
+
+      mf1 => nubar_mat%mf1
+      do while(associated(mf1))
+         if(mf1%mt .eq. 456) exit
+	 mf1 => mf1%next
+      end do
+      
+      if(.not.(associated(mf1))) then
+          write(8,*) ' MT456 not found in ',infile(1:nin)
+          stop ' Aborted in readNubar'
+      endif 
+      
+      if(mf1%mt456%lnu .eq. 2) then
+        np = mf1%mt456%tb%np
+        eniu_eval = 0.0d0
+        vniu_eval = 0.0d0
+	if(np .gt. ndecse) then
+	    write(8,*) ' WARNING: # nubar energies > storage array'
+	    np = ndecse
+	endif
+c	Assigning energies to eniu_eval and converting to MeV
+        eniu_eval(1:np) = mf1%mt456%tb%dat%x/1.d6
+c	Assigning nubars to vniu_eval
+        vniu_eval(1:np) = mf1%mt456%tb%dat%y
+	num_niu = np
+       else
+        write(8,*) 'ERROR Reading nubar!'
+	write(8,*) 'Only option lnu=2 in MT=456 has been implemented!'
+	stop ' Abort in readNubar'
+      endif
+      
+      call del_mf(mf1)
+
+      return      
+      END
