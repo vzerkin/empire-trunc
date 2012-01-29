9bdb9548e67f46bef35440891c899c0f2fa46a0e
Roberto Capote <rcapotenoy@yahoo.com>
2012-01-30 00:05:24 +0100

    svn:2371 Sam and Gustavo, pls check the coding !!. Modified to read the nubar ENDF file from \data\nubar.endf, and retrieve the proper mat number according to the fissioning compound information provided by EMPIRE. Error management introduced by assigning a new dummy parameter ierr (ierr =0 sucess, ierr>0 failure). No stops inside the routine.

--- source/read_nubar.f
+++ source/read_nubar.f
@@ -2,8 +2,17 @@ Ccc   * $Rev: 2355 $
 Ccc   * $Author: rcapote $
 Ccc   * $Date: 2012-01-28 09:34:54 +0100 (Sat, 28 Jan 2012) $
 
-      SUBROUTINE READNUBAR(infile,nin)
-
+      SUBROUTINE READNUBAR(infile,nin,ierr,ia,iz)
+C
+C     Retrieves MF=1 MT=456 for an ENDF file material with A=ia, Z=iz 
+C
+C     ierr = 0 if success, file NUBAR-EVAL.ENDF created in the current directory
+C
+C     ierr = 1 if MF=1 MT=456 for a material with A=ia, Z=iz not found
+C
+C     Sam: Some additional coding is needed here to point to the right material
+C     before retrieving MF=1 MT=456 data 
+C
       use endf_io
 
       INCLUDE 'dimension.h'
@@ -11,45 +20,74 @@ Ccc   * $Date: 2012-01-28 09:34:54 +0100 (Sat, 28 Jan 2012) $
 
       integer*4, intent(in) :: nin
       character*200, intent(in) :: infile
-
+      integer ierr, ia, iz, iza 
       integer*4 np
 
       type (endf_mat) nubar_mat
+      type (endf_mat) , pointer :: p_nubar_mat
       type (mf_1), pointer :: mf1
       
+      ierr = 0 
+
+      iza = iz*1000 + ia
+
       call clear_mat(nubar_mat)
-      allocate(nubar_mat%mf1)
+      allocate(nubar_mat)
 
       call find_mf(infile(1:nin),nubar_mat)
+C
+C     Checking the right material
+C        with mf1%za = iza
+C
+      p_nubar_mat => nubar_mat
+      mf1 => p_nubar_mat%mf1
+      do while(associated(p_nubar_mat))
 
-      mf1 => nubar_mat%mf1
-      do while(associated(mf1))
-         if(mf1%mt .eq. 456) exit
-       mf1 => mf1%next
-      end do
-      
+         if(p_nubar_mat%mf1%mt .eq. 451) then 
+C          retrieving iza (e.g. 9.023100+4)
+           if( NINT(p_nubar_mat%mf1%mt451%za) .eq. iza  .and. 
+     &          p_nubar_mat%mf1%mt .eq. 456 ) then
+             mf1 => p_nubar_mat%mf1
+	       exit
+           endif 
+
+         endif  
+
+	   p_nubar_mat => p_nubar_mat%next
+
+      enddo
+    
       if(.not.(associated(mf1))) then
-          write(8,*) ' MT456 not found in ',infile(1:nin)
-          stop ' Aborted in readNubar'
+          write(8,*) ' WARNING: MT456 not found in ',infile(1:nin)
+          write(8,*) ' WARNING: for fissioning nucleus (IZA):',iza
+          write(8,*) ' WARNING: Evaluated nubar will not be available'
+          ierr = 1
+          return
       endif 
       
       if(mf1%mt456%lnu .eq. 2) then
         np = mf1%mt456%tb%np
         eniu_eval = 0.0d0
         vniu_eval = 0.0d0
-      if(np .gt. ndecse) then
-          write(8,*) ' WARNING: # nubar energies > storage array'
-          np = ndecse
-      endif
-c     Assigning energies to eniu_eval and converting to MeV
+        if(np .gt. NDEPFN) then
+          write(8,*) ' WARNING: # nubar energies > NDEPFN in dimension.h'
+          np = NDEPFN 
+        endif
+
+c       Assigning energies to eniu_eval and converting to MeV
         eniu_eval(1:np) = mf1%mt456%tb%dat%x/1.d6
-c     Assigning nubars to vniu_eval
+c       Assigning nubars to vniu_eval
         vniu_eval(1:np) = mf1%mt456%tb%dat%y
-      num_niu = np
-       else
-        write(8,*) 'ERROR Reading nubar!'
-      write(8,*) 'Only option lnu=2 in MT=456 has been implemented!'
-      stop ' Abort in readNubar'
+        num_niu = np
+
+      else
+
+        write(8,*) ' WARNING: Reading nubar (MT=456) in ',infile(1:nin)
+        write(8,*) ' WARNING: Only lnu=2 reading in MT=456 implemented!'
+        write(8,*) ' WARNING: Evaluated nubar will not be available'
+        ierr = 1
+        return
+
       endif
       
       call del_mf(mf1)
