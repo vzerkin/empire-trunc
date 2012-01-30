aa5c724e02c5cb8fdb3c534fc8d0a4bd10ceb4d7
Gustavo Nobre <gnobre@bnl.com>
2012-01-30 19:41:22 +0100

    svn:2389 File read_nubar.f modified by R. Capote. It still has issues, I will work on fixing them, but now it should compile (a temporary solution)

--- source/read_nubar.f
+++ source/read_nubar.f
@@ -20,46 +20,35 @@ C
 
       integer*4, intent(in) :: nin
       character*200, intent(in) :: infile
-      integer ierr, ia, iz, iza 
+      integer ierr, ia, iz, izatmp 
       integer*4 np
 
       type (endf_mat) nubar_mat
-      type (endf_mat) , pointer :: p_nubar_mat
+      !type (mat), pointer :: p_nubar_mat
       type (mf_1), pointer :: mf1
-      
+
       ierr = 0 
 
-      iza = iz*1000 + ia
+      izatmp = iz*1000 + ia
 
       call clear_mat(nubar_mat)
-      allocate(nubar_mat)
+      allocate(nubar_mat%mf1)
 
       call find_mf(infile(1:nin),nubar_mat)
 C
 C     Checking the right material
 C        with mf1%za = iza
 C
-      p_nubar_mat => nubar_mat
-      mf1 => p_nubar_mat%mf1
-      do while(associated(p_nubar_mat))
-
-         if(p_nubar_mat%mf1%mt .eq. 451) then 
-C          retrieving iza (e.g. 9.023100+4)
-           if( NINT(p_nubar_mat%mf1%mt451%za) .eq. iza  .and. 
-     &          p_nubar_mat%mf1%mt .eq. 456 ) then
-             mf1 => p_nubar_mat%mf1
-	       exit
-           endif 
-
-         endif  
-
-	   p_nubar_mat => p_nubar_mat%next
-
+      mf1 => nubar_mat%mf1
+      do while(associated(mf1))
+C        retrieving %za = izatmp (e.g. 9.023100+4)
+         if( NINT(mf1%mt456%za) .eq. izatmp) exit
+         mf1 => mf1%next
       enddo
     
-      if(.not.(associated(mf1))) then
+      if( .not.(associated(mf1) )) then
           write(8,*) ' WARNING: MT456 not found in ',infile(1:nin)
-          write(8,*) ' WARNING: for fissioning nucleus (IZA):',iza
+          write(8,*) ' WARNING: for fissioning nucleus (IZA):',izatmp
           write(8,*) ' WARNING: Evaluated nubar will not be available'
           ierr = 1
           return
@@ -70,7 +59,7 @@ C          retrieving iza (e.g. 9.023100+4)
         eniu_eval = 0.0d0
         vniu_eval = 0.0d0
         if(np .gt. NDEPFN) then
-          write(8,*) ' WARNING: # nubar energies > NDEPFN in dimension.h'
+          write(8,*)' WARNING: # nubar energies > NDEPFN in dimension.h'
           np = NDEPFN 
         endif
 
@@ -90,7 +79,43 @@ c       Assigning nubars to vniu_eval
 
       endif
       
-      call del_mf(mf1)
+      call del_mf(mf1) 
 
       return      
       END
+
+      real*8 FUNCTION fniu_nubar_eval(entmp)
+
+      real*8 entmp 
+
+      INCLUDE 'dimension.h'
+      INCLUDE 'global.h'
+C
+C     Getting global values from common blocks
+C     eniu_eval
+C     vniu_eval
+C     num_niu 
+
+      integer i
+ 
+      fniu_nubar_eval = vniu_eval(1)
+      if(entmp.lt.1.d-11) RETURN
+
+      if(entmp.gt.eniu_eval(num_niu)) then
+         WRITE(8,*) 
+     &   ' WARNING: In NUBAR reading, the incident Einc=', sngl(entmp),
+     &   ' > Emax_ENDF=', eniu_eval(num_niu)
+         WRITE(8,*) ' WARNING: Assumed that nubar(E) = nubar(Emax_ENDF)'
+         fniu_nubar_eval = vniu_eval(num_niu)
+         return
+      endif
+      
+      do i=1,num_niu
+        if(eniu_eval(i).gt.entmp) exit
+      enddo
+      fniu_nubar_eval = vniu_eval(i-1) +
+     &   (vniu_eval(i)-vniu_eval(i-1))*(entmp-eniu_eval(i-1))/
+     &   (eniu_eval(i)-eniu_eval(i-1))
+
+      return
+      end
