51cffe0d31a046bcff261afede95281ec468c188
David Brown <dbrown@bnl.gov>
2012-02-13 16:35:47 +0100

    svn:2561 fix stupid bug that caused windows zvview to get linked in on the mac

--- util/c4zvd/pickZVView.py
+++ util/c4zvd/pickZVView.py
@@ -2,5 +2,5 @@ import os
 
 if __name__ == "__main__":
     if os.uname()[0] == "Linux":    os.symlink( "zvv2-1.005-lin.exe", "zvview.exe" )
-    elif os.uname()[1] == "Darwin": os.symlink( "zvv2-1.005-mac.exe", "zvview.exe" )
+    elif os.uname()[0] == "Darwin": os.symlink( "zvv2-1.005-mac.exe", "zvview.exe" )
     else:                           os.symlink( "zvv2-1.005-win.exe", "zvview.exe" )
