578da3b118099bea7121e17789da41d3c97d2e33
Andrej Trkov <A.Trkov@iaea.org>
2012-03-16 15:03:51 +0100

    svn:2712 PLTLST - minor improvements

--- util/pltlst/pltlst.f
+++ util/pltlst/pltlst.f
@@ -17,6 +17,8 @@ C-V  2008/02 Add metastable products (MF10) (A. Trkov).
 C-V  2009/01 Add option to force major cross sections and/or 
 C-V          double-differential spectra to the list (A. Trkov).
 C-V  2009/02 Fix small bug in sequencing the forced entries.
+C-V  2011/11 Fix ZA of projectile for added reactions.
+C-V  2012/03 Guard cosines>1 in C4 - interpret as degrees
 C-M
 C-M  Manual for Program PLTLST
 C-M  -------------------------
@@ -71,11 +73,12 @@ C-
      8 ,'Lu','Hf','Ta','W ','Re','Os','Ir','Pt','Au','Hg'
      9 ,'Tl','Pb','Bi','Po','At','Rn','Fr','Ra','Ac','Th'
      * ,'Pa','U ','Np','Pu','Am','Cm','Bk','Cf','Es','Fm'/
+C* Tolerances for grouping data sets
 C* ETOL Fractional tolerance for differentiating incident energies of
 C*      differential and double differential data.
 C* ITOL Angle difference for differentiating energy distributions at
 C*      fixed angles.
-C* MTOL Angle difference for differentiating energy-dependent
+C* MTOL Angle difference (degrees) for differentiating energy-dependent
 C*      cross sections at fixed angles.
       DATA ETOL,ITOL,MTOL/ 0.015, 5, 1 /
       DATA BL10/'          '/
@@ -96,6 +99,27 @@ C*      cross sections at fixed angles.
         KNG(I)=0
         ANS(I)=0
       END DO
+      IZI1=0
+      IZA1=0
+      IZP1=1
+      MS1 =' '
+      MF1 =0
+      MT1 =0
+      CHA1=' '
+      CHB1=' '
+      CHC1=' '
+      ENR1=0
+      DEN1=0
+      XSR1=0
+      DXS1=0
+      PRA1=0
+      PRB1=0
+      LVL1=0
+      ELV1=0
+      REF1=0
+      NEN1=0
+      NSU1=0
+      IZIX=0
 C* Write the banner
       WRITE(LTT,903) BLNK
       WRITE(LTT,903) ' PLTLST - Generate listing of EXFOR data'
@@ -115,6 +139,8 @@ C* If input does not exist, try reading the default input
       READ (LIN,903,END=10) FLNM
       IF(FLNM.NE.BLNK) FLLS=FLNM
 C* Read flags until blank or EOF
+      IF(.NOT. EXST)
+     &WRITE(LTT,903) ' Enter additional commands (end=CTRL^Z) '
     8 READ (LIN,903,END=10) FLNM
       IF(FLNM.EQ.BLNK) GO TO 10
       IF(FLNM(1:6).EQ.'no4000') THEN
@@ -146,41 +172,82 @@ C* Read the first C4 record
       IF(REC(1:40).EQ.BLNK) GO TO 80
       READ (REC,902) IZI0,IZA0,MS0,MF0,MT0,CHA0,CHB0,ENR0,DEN0,XSR0,DXS0
      &              ,PRA0,PRB0,PRC0,PRD0,CHC0,REF0,NEN0,NSU0
+      NAN=0
+      MAN=0
       IZP0=1
+      LVL0=0
+      ELV0=0
+      ELZ0=0
       IF(MT0.EQ.   2) IZP0=IZI0
+      IF(MT0.EQ.   4) IZP0=IZI0
+      IF(MT0.EQ. 102) IZP0=   0
+      IF(MT0.EQ. 103) IZP0=1001
+      IF(MT0.EQ. 601) IZP0=1001
+      IF(MT0.EQ. 104) IZP0=1002
+      IF(MT0.EQ. 105) IZP0=1003
+      IF(MT0.EQ. 106) IZP0=2003
+      IF(MT0.EQ. 107) IZP0=2004
+      IF(MT0.EQ. 801) IZP0=2004
       IF(MT0.EQ.9000) IZP0=PRB0
+      IF(MT0.EQ.51 .OR. MT0.EQ.601 .OR. MT0.EQ.801) LVL0=1
+      IF(PRC0 .NE. 0) THEN
+C*      -- Define level energy or range (if applicable)
+        LVL0=1
+        ELV0=PRC0
+        ELZ0=0
+        IF(PRD0.GT.0) THEN
+          ELV0=MAX(PRC0,PRD0)
+          IF(MT0.NE.9000) ELZ0=MIN(PRC0,PRD0)
+        END IF
+      END IF
       IF(CHA0.EQ.'T' .OR. CHA0.EQ.'+') CHA0=' '
       IF(CHA0.EQ.'1') CHA0='M'
       IF(CHA0.EQ.'2') CHA0='N'
       IF(CHA0.EQ.'3') CHA0='O'
-      NAN=0
-      MAN=0
-      LVL0=0
-      IF(PRC0 .NE. 0) LVL0=1
-      IF(MT0.EQ.51 .OR. MT0.EQ.601 .OR. MT0.EQ.801) LVL0=1
 C*
 C* Process all C4 records and check for changes
    20 CONTINUE
       IEF =1
       MMF =99
       MMT =999
-      READ (LEX,901,END=42) REC
-      IF(REC(1:40).EQ.BLNK) GO TO 42
+      READ (LEX,901,END=40) REC
+      IF(REC(1:40).EQ.BLNK) GO TO 40
       READ (REC,902) IZI1,IZA1,MS1,MF1,MT1,CHA1,CHB1,ENR1,DEN1,XSR1,DXS1
      &              ,PRA1,PRB1,PRC1,PRD1,CHC1,REF1,NEN1,NSU1
       IEF =0
       IZP1=1
       LVL1=0
-      IF(PRC1 .NE. 0) LVL1=1
-      IF(MT1.EQ.51 .OR. MT1.EQ.601 .OR. MT1.EQ.801) LVL1=1
+      ELV1=0
+      ELZ1=0
+      IF(MT1.EQ. 102) IZP1=   0
+      IF(MT1.EQ. 103) IZP1=1001
+      IF(MT1.EQ. 104) IZP1=1002
+      IF(MT1.EQ. 105) IZP1=1003
+      IF(MT1.EQ. 106) IZP1=2003
+      IF(MT1.EQ. 107) IZP1=2004
+      IF(MT0.EQ.9000) IZP1=PRB1
       IF(MT1.EQ.   2) IZP1=IZI1
-      IF(MT1.EQ.9000) IZP1=PRB1
+      IF(MT1.EQ.   4) IZP1=IZI1
+      IF(MT1.EQ.  51) IZP1=IZI1
+      IF(MT1.EQ. 601) IZP1=1001
+      IF(MT1.EQ. 801) IZP1=2004
+      IF(MT1.EQ.51 .OR. MT1.EQ.601 .OR. MT1.EQ.801) LVL1=1
+      IF(PRC1 .NE. 0) THEN
+C*      -- Define level energy or range (if applicable)
+        LVL1=1
+        ELV1=PRC1
+        ELZ1=0
+        IF(PRD1.GT.0) THEN
+          ELV1=MAX(PRC1,PRD1)
+          IF(MT1.NE.9000) ELZ1=MIN(PRC1,PRD1)
+        END IF
+      END IF
       IF(CHA1.EQ.'T' .OR. CHA1.EQ.'+') CHA1=' '
       IF(CHA1.EQ.'1') CHA1='M'
       IF(CHA1.EQ.'2') CHA1='N'
       IF(CHA1.EQ.'3') CHA1='O'
 C* Ignore MT 51 without specified level energies
-      IF(MT1.EQ.51 .AND. PRC1.EQ.0) GO TO 20
+      IF(MT1.EQ.51 .AND. ELV1.EQ.0) GO TO 20
 C* Mark for printout if any of the parameters change
 c...
 C...  if(mf1.eq.3 .and. mt1.eq.102) print *,'mf1,mt1,m0,m1'
@@ -193,10 +260,17 @@ c...
       IF(MT1 .NE.MT0 ) GO TO 40
       IF(CHA0.NE.CHA1) GO TO 40
 C* Mark partial inelastic MF 3 where level energy changes
-      IF(MF1 .EQ. 3 .AND. PRC1.NE.PRC0) GO TO 40
+      IF(MF1 .EQ. 3 .AND. (ELV1.NE.ELV0 .OR. ELZ1.NE.ELZ0)) GO TO 40
 C* Mark ang.distr. MF 4 where level energy changes and save angle
       IF(MF1 .EQ. 4) THEN
-        ANJ=ACOS(PRA1)*180/PI
+        IF(ELV1.NE.ELV0 .OR. ELZ1.NE.ELZ0) GO TO 40
+        IF(PRA1.GT.1) THEN
+          ANJ=PRA1
+          WRITE(LTT,905) ' PLTLST WARNING - Angular cosine        ',ANJ
+          WRITE(LTT,905) '                  assumed to be degrees '
+        ELSE
+          ANJ=ACOS(PRA1)*180/PI
+        END IF
         JAN=NINT(ANJ)
         NEW=1
         IF(MAN.GT.0) THEN
@@ -231,7 +305,6 @@ C* Mark ang.distr. MF 4 where level energy changes and save angle
           ANS(NAN)=ANJ
           KAN(NAN)=1
         END IF
-        IF(PRC1.NE.PRC0) GO TO 40
       END IF
 C* Mark spectra MF 5 where outgoing particle angle changes
       IF(MF1 .EQ. 5 .AND. PRA1.NE.PRA0) GO TO 40
@@ -259,12 +332,15 @@ C* - MT out of range
       IF(MT0.GT.999 .AND. MT0.NE.9000) GO TO 60
 C* - Insufficient number of points (this also excludes distributions
 C*   which are not suitably sorted and would result in excessive output)
+c...
+C...  print *,'iex,mf0',iex,mf0
+c...
 c...  IF(IEX.LE.2) GO TO 60
       IF(IEX.LE.2 .AND. (MF0.GT.3 .AND. MF0.NE.10)) GO TO 50
 C*
 C* Printout conditions satisfied - prepare output record
-      IZ=IZA1/1000
-      IA=IZA1-1000*IZ
+      IZ=IZA0/1000
+      IA=IZA0-1000*IZ
       DO I=1,3
         CH10(I)='          '
       END DO
@@ -273,7 +349,13 @@ C* Consider different cases
       MMT=MT0
       IF     (MF0.EQ.3) THEN
 C* - MF3 discrete level energy (if present)
-        IF(LVL0.NE.0) WRITE(CH10(3),'(1P,E10.4E1)') PRC0
+        IF(LVL0.NE.0) THEN
+          WRITE(CH10(3),'(1P,E10.4E1)') ELV0
+          IF(ELZ0.GT.0) THEN
+            WRITE(CH10(2),'(1P,E8.2E1)') ELZ0
+            CH10(3)(1:1)='+'
+          END IF
+        END IF
         IF(CHA0.NE.' ') THEN
             MMF=10
             IF     (CHA0.EQ.'G') THEN
@@ -291,7 +373,13 @@ C* - MF3 discrete level energy (if present)
       ELSE IF(MF0.EQ.4) THEN
 C* - MF4 incident particle energy and level
         WRITE(CH10(1),'(1P,E10.3E1)') ENR0
-        IF(LVL0.NE.0) WRITE(CH10(3),'(1P,E10.4E1)') PRC0
+        IF(LVL0.NE.0) THEN
+          WRITE(CH10(3),'(1P,E10.4E1)') ELV0
+          IF(ELZ0.GT.0) THEN
+            WRITE(CH10(2),'(1P,E8.2E1)') ELZ0
+            CH10(3)(1:1)='+'
+          END IF
+        END IF
       ELSE IF(MF0.EQ.5) THEN
 C* - MF 5 incident particle energy
         WRITE(CH10(1),'(1P,E10.3E1)') ENR0
@@ -348,9 +436,18 @@ C*      -- Set output record
         NXSMJR=NXSMJR+1
         IF(MTX.EQ.18 .AND.IZX.LT.90) GO TO 48
         MFX   =3
-        IZPX  =1
-        IZIX  =0
-        IZIX  =1
+        IZIX  =IZI0
+        IF     (MTX.EQ.  1) THEN
+          IZPX  =0
+        ELSE IF(MTX.EQ.  4) THEN
+          IZPX  =IZIX
+        ELSE IF(MTX.EQ. 16) THEN
+          IZPX  =1
+        ELSE IF(MTX.EQ.102) THEN
+          IZPX  =0
+        ELSE
+          IZPX  =0
+        END IF
         CX10(1)='          '
         CX10(2)='          '
         CX10(3)='          '
@@ -358,6 +455,8 @@ C*      -- Set output record
       END IF
 C*
    44 IF(NDDXN.GT.0 .AND. NDDXN.LT.MXDDE) THEN
+      IF( MMF.LT.6) GO TO 48
+      IF((MMF.EQ.6 .AND. MMT.LT.9000) .OR. IZI0.GT.1) GO TO 48
 C* Force double-differential spectra listing
         IF(IEF.NE.0) THEN
           IF(IDX.LE.0) STOP 'PLTLST ERROR - No data in C4 file'
@@ -377,10 +476,10 @@ C* Force double-differential spectra listing
         END IF
         GO TO 48
 C*      -- Set output record
-   46   IZPX  =1
+   46   IF(IZIX.EQ.0) IZIX=IZI0
+        IZPX  =IZIX
         MFX   =6
         MTX   =9000
-        IZIX  =1
         WRITE(CX10(1),'(1P,E10.3E1)') EDDX(NDDXN)
         WRITE(CX10(2),'(F8.2)')       ADDX(NDDXA)
         CX10(3)='          '
@@ -398,14 +497,28 @@ C*        -- Force neutron emission spectra
      &                  ,CX10,IDX,IZIX
           GO TO 42
       END IF
-      IF(IEF.NE.0) GO TO 80
+      IF(MMF.GE.99) GO TO 80
 C* Write a record to output list file
-      IDX=IDX+1
-      WRITE(LLS,914) IZ,CH(IZ),IA,MS0,IZP0,MMF,MMT,IEX,CH10,IDX,IZI0
+C* Suppress elastic angular distributions for charged particles
+      IF(MMF.NE.4 .OR. MMT.NE.2 .OR. IZI0.LE.1) THEN
+        IDX=IDX+1
+        WRITE(LLS,914) IZ,CH(IZ),IA,MS0,IZP0,MMF,MMT,IEX,CH10,IDX,IZI0
+      END IF
+      MMF=99
+      MMT=999
       IZX=IZ
       IAX=IA
       MSX=MS0
-   50 IF(NO4000.EQ.0 .AND. MF0.EQ.4 .AND.
+   50 CONTINUE
+C...
+C...  print *,'NO4000,mf0,mf1,mt0,mt1,ief',NO4000,mf0,mf1,mt0,mt1,ief
+C...
+C* Override NO4000 for incident charged particles
+C...  IF(NO4000.EQ.1) GO TO 60
+      IF(NO4000.EQ.1 .AND.
+     &  (IZI0.LE.1 .OR. MF0.NE.4 .OR. MT0.NE.2)) GO TO 60
+C*
+      IF( MF0.EQ.4 .AND.
      &   (MF1.GT.MF0 .OR. MT1.NE.MT0 .OR. IEF.EQ.1)) THEN
         CH10(1)='          '
 C* Use fine angular mesh for 40000 series
@@ -451,12 +564,13 @@ C* Reset parameters
       PRA0=PRA1
       PRB0=PRB1
       LVL0=LVL1
-      PRC0=PRC1
-      PRD0=PRD1
+      ELV0=ELV1
+      ELZ0=ELZ1
       REF0=REF1
       NEN0=NEN1
       NSU0=NSU1
-      IF(IEF.NE.1) GO TO 20
+      IF(IEF.EQ.1) GO TO 42
+      GO TO 20
 C* All processing completed
    80 WRITE(LLS,910)
       WRITE(LTT,903) BLNK
@@ -466,6 +580,7 @@ C*
   902 FORMAT(I5,I6,A1,I3,I4,2A1,1X,8F9.0,A3,A26,I4,I3)
   903 FORMAT(2A40)
   904 FORMAT(BN,F10.0)
+  905 FORMAT(A40,1P,E10.3)
   910 FORMAT(' ======================================='
      &      ,'====================================  ======')
   912 FORMAT(' MATERIAL   ZAOUT  MF   MT  EVAL. EXPR. '
