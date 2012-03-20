9e6a9cb447d40e223ff1460a932cd2b98ab10d9f
David Brown <dbrown@bnl.gov>
2012-03-20 17:50:34 +0100

    svn:2723 util not utils

--- util/c4zvd/zvview.py
+++ util/c4zvd/zvview.py
@@ -28,9 +28,9 @@ def X_is_running():
 if __name__ == "__main__":
     
     # Get correct version of ZVView for this machine
-    if os.uname()[0] == "Linux":    zvviewExe = os.sep.join( [ zvviewEnv[ 'EMPIREDIR' ], 'utils', 'c4zvd', "zvv2-1.005-lin.exe" ] )
-    elif os.uname()[0] == "Darwin": zvviewExe = os.sep.join( [ zvviewEnv[ 'EMPIREDIR' ], 'utils', 'c4zvd', "zvv2-1.005-mac.exe" ] )
-    else:                           zvviewExe = os.sep.join( [ zvviewEnv[ 'EMPIREDIR' ], 'utils', 'c4zvd', "zvv2-1.005-win.exe" ] )
+    if os.uname()[0] == "Linux":    zvviewExe = os.sep.join( [ zvviewEnv[ 'EMPIREDIR' ], 'util', 'c4zvd', "zvv2-1.005-lin.exe" ] )
+    elif os.uname()[0] == "Darwin": zvviewExe = os.sep.join( [ zvviewEnv[ 'EMPIREDIR' ], 'util', 'c4zvd', "zvv2-1.005-mac.exe" ] )
+    else:                           zvviewExe = os.sep.join( [ zvviewEnv[ 'EMPIREDIR' ], 'util', 'c4zvd', "zvv2-1.005-win.exe" ] )
                                     
     # Make any adjustments to the run-time environment & make sure X11 is running
     if os.uname()[0] in [ "Darwin", "Linux" ]:
