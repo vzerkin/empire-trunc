2406ea72b936edff9b4c34e793603e1241d43e83
Gustavo Nobre <gnobre@bnl.com>
2012-04-12 22:14:17 +0200

    svn:2789 Update to file 'util/kalman/pnt2kal.f'. Writeouts of kalman inputs were converted into a separate subroutine. Also eliminated some unused variables.

--- util/kalman/pnt2kal.f
+++ util/kalman/pnt2kal.f
@@ -42,9 +42,8 @@ C     into the format needed by kalman. This is neede for running kalman for PFN
       CHARACTER*60 pntfile, proj, inpsen
       CHARACTER*6 param
       INTEGER*4 MAXEXP ! Number of experiments
-      INTEGER*4 iexp, idat, nexp,MT1,MAT,NEX, nparam, ierr,MS,i,nblank
+      INTEGER*4 nexp,MT1,MAT,NEX, nparam, ierr,MS
       PARAMETER(MAXEXP=500)
-      REAL*8 up, down, norm
 
       TYPE (DATASET) EXPDATA(MAXEXP)
 
@@ -85,88 +84,8 @@ c     Counting the number of parameters in sensitivity input file
 
 
 
-C       Writing spectra in file fort.10, relative uncertainties in 
-C       file fort.11, and default experimental correlations in file fort.12
-C       (emitted energies will be printed in MeV)
 
-C     Writing input for Kalman
-
-
-
-      do iexp=1, NEXP
-
-c       Writing the headers
-        write(10,200) EXPDATA(iexp)%REF,EXPDATA(iexp)%KENTRY,
-     & EXPDATA(iexp)%KSUBENT, EXPDATA(iexp)%NDAT
-        write(11,200) EXPDATA(iexp)%REF,EXPDATA(iexp)%KENTRY,
-     & EXPDATA(iexp)%KSUBENT, EXPDATA(iexp)%NDAT
-        write(12,200) EXPDATA(iexp)%REF,EXPDATA(iexp)%KENTRY,
-     & EXPDATA(iexp)%KSUBENT, -EXPDATA(iexp)%NDAT
-200     format(A25,5X,A5,I3,5X,I5)
-        if(EXPDATA(iexp)%Einc.GT.1.D-20) then
-c         Normalized spectra
-          norm = 1.D0
-        else
-c         Cross section: Converting from barns to mb
-          norm = 1.D3
-        endif
-c       Writing while converting emitted energy from eV to MeV
-        do idat=1,EXPDATA(iexp)%NDAT
-          if(EXPDATA(iexp)%DAT(idat)%VAL.LT.1.D-15) then
-           EXPDATA(iexp)%DAT(idat)%relaerr = 0.0D0
-          else
-           EXPDATA(iexp)%DAT(idat)%relaerr = 
-     & EXPDATA(iexp)%DAT(idat)%ERR/
-     & EXPDATA(iexp)%DAT(idat)%VAL
-          endif
-        enddo    ! data-points loop
-        write(10,220) (EXPDATA(iexp)%DAT(idat)%E*1.D-6, 
-     & EXPDATA(iexp)%DAT(idat)%VAL*norm,
-     & idat=1,EXPDATA(iexp)%NDAT)
-        write(11,220) (EXPDATA(iexp)%DAT(idat)%E*1.D-6, 
-     & EXPDATA(iexp)%DAT(idat)%relaerr, idat=1,EXPDATA(iexp)%NDAT)
-220     format(6(1PE11.4))
-        write(12,*) '0.200'
-      enddo   ! experiments loop
-
-
-
-
-
-
-C     Writing Kalman input (to be thrown into file 'KALMAN.INP')
-C     (based on c4tokal.f)
-      WRITE(6,*)  'INPUT'
-      WRITE(6,300) MS,NPARAM,0,0,1,1.0,0.0,0.0
-      WRITE(6,310)(I,I=1,NPARAM)
-300   FORMAT(5I5,5X,3E10.3)
-310   FORMAT(14I5)
-      do iexp=1, NEXP
-c       The first number one below corresponds do variable J2 in c4tokal. It is used
-c       in case there are more reactions/MTs in xsec and sensitivity files. In the case of PFNS
-c       it should later on correspond to additional incident energies. 
-c       write(6,350) 4, 1 
-        write(6,350) 1, 1 
-350     format(2I5)
-c       Again, the line below should also be modified when this code is generalized to handle 
-c       additional incident energies. (see line 125 of c4tokal (rev. 2719)
-        if(MT1.eq.EXPDATA(iexp)%MT.and.nex.eq.1) then
-          write(6,360) 1.0, EXPDATA(iexp)%REF
-360       format(1PE10.3,5X,A25)
-        else
-          write(6,370) 0.0
-370       format(1PE10.3)
-        endif
-
-c       Writing in fort.75 for plotting of experimental data
-        do idat=1,EXPDATA(iexp)%NDAT
-          if(EXPDATA(iexp)%DAT(idat)%ERR.GT.1.D-15) write(75,400) 
-     & EXPDATA(iexp)%DAT(idat)%E*1.D-6,
-     & EXPDATA(iexp)%DAT(idat)%VAL,EXPDATA(iexp)%DAT(idat)%ERR,
-     & EXPDATA(iexp)%MT
-400     FORMAT(3(1X,E12.5),I4)
-        enddo
-      enddo
+      CALL WRITEKAL(EXPDATA,NEXP,MAXEXP,MS,nparam,MT1,nex)
 
 
       END
@@ -230,6 +149,16 @@ c       Units are eV and barns for cross sections and eV and unitless for spectr
      & up,down
 50        format(D11.5,22X,3(D11.3))
           EXPDATA(iexp)%DAT(idat)%ERR = (up+down)/2.d0
+
+c         Calculating relative errors
+          if(EXPDATA(iexp)%DAT(idat)%VAL.LT.1.D-15) then
+           EXPDATA(iexp)%DAT(idat)%relaerr = 0.0D0
+          else
+           EXPDATA(iexp)%DAT(idat)%relaerr = 
+     & EXPDATA(iexp)%DAT(idat)%ERR/
+     & EXPDATA(iexp)%DAT(idat)%VAL
+          endif
+
         enddo
 100     CONTINUE
         BACKSPACE(100)
@@ -242,5 +171,114 @@ c       Making it an error to have exp. sets with 0 points
       enddo
 150   CONTINUE
       NEXP = iexp - 1
+
+      END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      SUBROUTINE WRITEKAL(EXPDATA,NEXP,MAXEXP,MS,nparam,MT1,nex)
+
+
+      use dataio
+
+      IMPLICIT NONE
+
+      INTEGER*4 iexp, MAXEXP, idat, nexp, nparam, MS, MT1, nex, i
+      REAL*8 norm
+      
+      TYPE (DATASET) EXPDATA(MAXEXP)
+
+
+
+C       Writing spectra in file fort.10, relative uncertainties in 
+C       file fort.11, and default experimental correlations in file fort.12
+C       (emitted energies will be printed in MeV)
+
+C     Writing input for Kalman
+
+
+
+      do iexp=1, NEXP
+
+c       Writing the headers
+        write(10,200) EXPDATA(iexp)%REF,EXPDATA(iexp)%KENTRY,
+     & EXPDATA(iexp)%KSUBENT, EXPDATA(iexp)%NDAT
+        write(11,200) EXPDATA(iexp)%REF,EXPDATA(iexp)%KENTRY,
+     & EXPDATA(iexp)%KSUBENT, EXPDATA(iexp)%NDAT
+        write(12,200) EXPDATA(iexp)%REF,EXPDATA(iexp)%KENTRY,
+     & EXPDATA(iexp)%KSUBENT, -EXPDATA(iexp)%NDAT
+200     format(A25,5X,A5,I3,5X,I5)
+        if(EXPDATA(iexp)%Einc.GT.1.D-20) then
+c         Normalized spectra
+          norm = 1.D0
+        else
+c         Cross section: Converting from barns to mb
+          norm = 1.D3
+        endif
+c       Writing while converting emitted energy from eV to MeV
+        write(10,220) (EXPDATA(iexp)%DAT(idat)%E*1.D-6, 
+     & EXPDATA(iexp)%DAT(idat)%VAL*norm,
+     & idat=1,EXPDATA(iexp)%NDAT)
+        write(11,220) (EXPDATA(iexp)%DAT(idat)%E*1.D-6, 
+     & EXPDATA(iexp)%DAT(idat)%relaerr, idat=1,EXPDATA(iexp)%NDAT)
+220     format(6(1PE11.4))
+        write(12,*) '0.200'
+      enddo   ! experiments loop
+
+
+
+
+
+
+C     Writing Kalman input (to be thrown into file 'KALMAN.INP')
+C     (based on c4tokal.f)
+      WRITE(6,*)  'INPUT'
+      WRITE(6,300) MS,NPARAM,0,0,1,1.0,0.0,0.0
+      WRITE(6,310)(I,I=1,NPARAM)
+300   FORMAT(5I5,5X,3E10.3)
+310   FORMAT(14I5)
+      do iexp=1, NEXP
+c       The first number one below corresponds do variable J2 in c4tokal. It is used
+c       in case there are more reactions/MTs in xsec and sensitivity files. In the case of PFNS
+c       it should later on correspond to additional incident energies. It is the index of 
+c       column that is being fitted (disregarding the first one, obviously, since it corresponds to the energy) 
+c       write(6,350) 4, 1 
+        write(6,350) 1, 1 
+350     format(2I5)
+c       Again, the line below should also be modified when this code is generalized to handle 
+c       additional incident energies. (see line 125 of c4tokal (rev. 2719)
+        if(MT1.eq.EXPDATA(iexp)%MT.and.nex.eq.1) then
+          write(6,360) 1.0, EXPDATA(iexp)%REF
+360       format(1PE10.3,5X,A25)
+        else
+          write(6,370) 0.0
+370       format(1PE10.3)
+        endif
+
+c       Writing in fort.75 for plotting of experimental data
+        do idat=1,EXPDATA(iexp)%NDAT
+          if(EXPDATA(iexp)%DAT(idat)%ERR.GT.1.D-15) write(75,400) 
+     & EXPDATA(iexp)%DAT(idat)%E*1.D-6,
+     & EXPDATA(iexp)%DAT(idat)%VAL,EXPDATA(iexp)%DAT(idat)%ERR,
+     & EXPDATA(iexp)%MT
+400     FORMAT(3(1X,E12.5),I4)
+        enddo
+      enddo
+
+
 
       END
