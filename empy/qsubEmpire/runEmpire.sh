dc3a1aad96c738cb1803047f7fc518b1acf4c59e
A. Palumbo <apalumbo@bnl.gov>
2012-05-25 17:29:13 +0200

    svn:2863 Modify runEmpire.sh to copy contents of working directory on nlc to working directory on local machine. Then after completing the run, copy all files back to directory on nlc.  - Sam

--- empy/qsubEmpire/runEmpire.sh
+++ empy/qsubEmpire/runEmpire.sh
@@ -9,13 +9,38 @@
 #
 #SCRIPT TO RUN FULL EMPIRE PACKAGE
 
+# for local testing
+# dir=/home/herman/temp
+# file=u235
+# clean=Y
+
 origDir=`pwd`
-cd $dir
+
+# create directory on local /dev/shm using local
+# directory and one level up.
+
+strt=${dir%/*/*}
+end=${dir#$strt}
+work=/dev/shm$end
+
+# make local working directory if not already there
+
+if [ ! -e $work ]; then
+   mkdir -p $work
+fi
+
+# copy files over to local directory
+
+rm -r $work/* 2>/dev/null
+cd $work
+cp -r $dir/* ./
 
 # RUN X-SEC CALCULATIONS & EXFOR PROCESSING
+
 ~/empire/scripts/runE $file
 
 # check if everything completed properly:
+
 if [ ! `tail -n 2 $file.out | grep -c "SUCCESSFULLY"` = 1 ]; then
     echo crash: $dir >> $origDir/crashlog.txt
 fi
@@ -27,5 +52,14 @@ if [ $clean = Y ]; then
     grep_pattern="$file.xsc\|$file-pfns.out"
     ls | grep -v $grep_pattern | xargs rm -rf
 fi
+
+# all done. Copy local directory back to original
+
+cp -r ./* $dir
+
+# clean up - remove working directory and parent (if empty)
+
+rm -r $work 2>/dev/null
+rmdir ${work%/*} 2>/dev/null
 
 exit
