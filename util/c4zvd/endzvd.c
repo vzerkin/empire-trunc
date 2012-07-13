09e70e02aca4fed2953c9ffd8588f7440a6d1837
Mike Herman <mike.w.herman@gmail.com>
2012-07-13 23:42:32 +0200

    svn:2926 Ensure that the curve label specified in the ZVV GUI is appears on the plot. It replaces previous default that was showing file name plus MF and MT that might be sometimes convenient but is generally not acceptable for publication. The change will affect only plots done

--- util/c4zvd/endzvd.c
+++ util/c4zvd/endzvd.c
@@ -12,18 +12,18 @@ FILE    *inFile, *outFile;
 char    *ss,*my_fgets();
 
 char  *nucl[] = {
-     "NN","H","HE","LI","BE","B","C","N","O"
-    ,"F","NE","NA","MG","AL","SI","P","S","CL","AR"
-    ,"K","CA","SC","TI","V","CR","MN","FE","CO","NI"
-    ,"CU","ZN","GA","GE","AS","SE","BR","KR","RB","SR"
-    ,"Y","ZR","NB","MO","TC","RU","RH","PD","AG","CD"
-    ,"IN","SN","SB","TE","I","XE","CS","BA","LA","CE"
-    ,"PR","ND","PM","SM","EU","GD","TB","DY","HO","ER"
-    ,"TM","YB","LU","HF","TA","W","RE","OS","IR","PT"
-    ,"AU","HG","TL","PB","BI","PO","AT","RN","FR","RA"
-    ,"AC","TH","PA","U","NP","PU","AM","CM","BK","CF"
-    ,"ES","FM","MD","NO","LR","RF","DB","SG","BH","HS"
-    ,"MT","*","*","*"
+     "n","H","He","Li","Be","B","C","N","O"
+    ,"F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar"
+    ,"K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni"
+    ,"Cu","Zn","Ga","Ge","As","Se","Br","Kr","Rb","Sr"
+    ,"Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd"
+    ,"In","Sn","Sb","Te","I","Xe","Cs","Ba","La","Ce"
+    ,"Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er"
+    ,"Tm","Yb","Lu","Hf","Ta","W","Re","Os","Ir","Pt"
+    ,"Au","Hg","Tl","Pb","Bi","Po","At","Rn","Fr","Ra"
+    ,"Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf"
+    ,"Es","Fm","Md","No","Lr","Rf","Db","Sg","Bh","Hs"
+    ,"Mt","*","*","*"
 };
 int nNucl;
 
@@ -111,9 +111,16 @@ char    **argv;
             }
 //	    printf("===[%s]\n",str0);
 	    makeNuclide(za,str1);
-//	    fprintf(outFile,"#begin %s/3\n",filename);
 //	    fprintf(outFile,"#begin %s:%s:MF%d:MT%d/3\n",filename,str1,mf,mt);
-            fprintf(outFile,"#begin %s:MF%d:MT%d/3\n",str1,mf,mt);
+//    	fprintf(outFile,"#begin %s:MF%d:MT%d/3\n",str1,mf,mt);
+    	fprintf(outFile,"#begin %s(MT%d)/3\n",str1,mt);
+
+//     	    if (filename=='') {
+//     	    	fprintf(outFile,"#begin %s:MF%d:MT%d/3\n",str1,mf,mt);
+//     	    }
+//     	    else {
+//         	    fprintf(outFile,"#begin %s/3\n",filename);
+//     	    }
             fprintf(outFile,"%s\n",str0);
             flagBegin=1;
             for (; ; ) {
@@ -157,7 +164,8 @@ int makeNuclide(int za, char *str)
     z=za/1000;
     a=za%1000;
     if ((z<0)||(z>=nNucl)) return(-1);
-    sprintf(str,"%d-%s-%d",z,nucl[z],a);
+//    sprintf(str,"%d-%s-%d",z,nucl[z],a);
+    sprintf(str,"%d%s",a,nucl[z]);
     return(0);
 }
 
