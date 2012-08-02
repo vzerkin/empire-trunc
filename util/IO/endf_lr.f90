033efc57c6ff852212c64a83638a9c038be9970d
Sam Hoblit <shoblit@bnl.gov>
2012-08-02 21:44:22 +0200

    svn:3018 bring fortran/io endf_lr.f90 interface to that of block-mode routine endf_blklin.f90.

--- util/IO/endf_lr.f90
+++ util/IO/endf_lr.f90
@@ -13,27 +13,43 @@ module endf_line_io
 
     integer*4, parameter :: lun = 20    ! fortran logical unit number
 
+    ! the following error codes are used in block-mode, but not supported here.
+    ! but we must define these to maintain an identical interface for low-level IO.
+
+    integer*4, parameter :: file_not_80    = -1000000   ! status code for file with record length .ne. 80
+    integer*4, parameter :: file_bad_read  = -2000000   ! status for bad number of read bytes
+    integer*4, parameter :: file_bad_write = -3000000   ! status for bad number of read bytes
+    integer*4 :: file_bytes_requested    ! for error reporting. Number of bytes requeseted for read/write
+    integer*4 :: file_bytes_receieved    ! for error reporting. Number of bytes recieved for read/write
+
+
     ! -----------  Public interface ------------------------------------------
 
     character*80, public :: endline     ! current line
     integer*4, public :: filin          ! file line #
     public open_endf_file, get_endf_line, put_endf_line, close_endf_file
+    public file_not_80, file_bad_read, file_bad_write, file_bytes_requested, file_bytes_receieved  ! error reporting
 
 !------------------------------------------------------------------------------
     contains
 !------------------------------------------------------------------------------
 
-    integer*4 function open_endf_file(efil,qwrite)
+    integer*4 function open_endf_file(efil,qwrite,iover)
 
     implicit none
 
     character*(*), intent(in) :: efil    ! endf file name
     logical*4, intent(in) :: qwrite
+    integer*4, intent(in) :: iover          ! set /= 0 to overwrite existing file
 
     integer*4 stat
 
     if(qwrite) then
-        open(lun,file=efil,action='write',status='new',iostat=stat)
+        if(iover == 0) then
+           open(lun,file=efil,action='write',status='new',iostat=stat)
+        else
+           open(lun,file=efil,action='write',status='unknown',iostat=stat)
+        endif
     else
         open(lun,file=efil,action='read',status='old',iostat=stat)
     endif
