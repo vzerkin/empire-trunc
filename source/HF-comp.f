d0021f03f3004f3703ba4a680c3a331090de3805
Mike Herman <mike.w.herman@gmail.com>
2012-11-09 21:37:47 +0100

    svn:3195 Correcting DOS EOL

--- source/HF-comp.f
+++ source/HF-comp.f
@@ -1,6 +1,6 @@
-Ccc   * $Rev: 3194 $
-Ccc   * $Author: rcapote $
-Ccc   * $Date: 2012-11-09 21:36:14 +0100 (Fr, 09 Nov 2012) $
+Ccc   * $Rev: 3195 $
+Ccc   * $Author: mherman $
+Ccc   * $Date: 2012-11-09 21:37:47 +0100 (Fr, 09 Nov 2012) $
 C
       SUBROUTINE ACCUM(Iec,Nnuc,Nnur,Nejc,Xnor)
       INCLUDE 'dimension.h'
