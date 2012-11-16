3bdb928fad96af1ea85da4bc590a391fb09aa083
Roberto Capote <rcapotenoy@yahoo.com>
2012-11-16 02:06:04 +0100

    svn:3227 Constant changed to use CODATA 2010 set Iliadis 0.989534 -> CETa * 2 * pi = 0.9895106848

--- source/S-factor.f
+++ source/S-factor.f
@@ -1,8 +1,8 @@
-ccc   * $Rev: 3212 $
-Cc    * $Author: rcapote $
-Ccc   * $Date: 2012-11-15 11:52:39 +0100 (Thu, 15 Nov 2012) $
-Cc    *
-CC    * Original formulation (3210) $Author: apalumbo$ (Wed, 14 Nov 2012) $
+ccc   $Rev: 3212 $
+Cc    $Author: rcapote $
+Ccc   $Date: 2012-11-15 11:52:39 +0100 (Thu, 15 Nov 2012) $
+C
+Cc    * Original formulation (3210) $Author: apalumbo$ (Wed, 14 Nov 2012) $
  
       REAL*8 FUNCTION SFACTOR(Csection)
 
@@ -10,20 +10,18 @@ CC  This program calculates the S-factor for a non-resonant reaction.
 CC  Input: Cross section Csection is in mb
 CC  The CMS energy EIN is taken from EMPIRE (in MeV)
 CC  S-factor is output in MeV barns (MeV*b). 
-CC  Treatment follows A.G.W.Cameron, Technical Report CRL-41, AECL-454 (Chalk River, Ontario, June 1957), p.30,31
-CC  Available online at http://www.fas.org/sgp/eprint/CRL-41.pdf 
-CC 
+CC
 CC  Treatment follows Cauldrons in the Cosmos - Rolfs and Rodney (University of Chicago Press - 1988) 
 CC  Outlined in Empire.pdf
 CC
-CC  A third treatment of the S-factor formulation follows from Nuclear Physics of Stars - Christian IliaCC  dis - (Wiley-Vch - 2007) 
+CC  Another treatment of the S-factor formulation follows from Nuclear Physics of Stars - Christian Iliadis - (Wiley-Vch - 2007) 
 CC
-CC  The calculation of eta is consistent in both Rolfs/Rodney and Iliadis.  Cameron's formulation is
-CC  causing slight deviations in the S-factor formulation (in two tested cases of the capture - 106Cd
-CC  and 120Te) to the third decimal place.  Since the Incident energy is in MeV, the formulation of 
-CC  eta by C. Iliadis will be used.
+CC  The calculation of eta is consistent in both Rolfs/Rodney and Iliadis.  
+CC  Since the Incident energy is in MeV, the formulation of eta by C. Iliadis is preferred.
 CC
-
+CC  RC: Iliadis constant changed to use EMPIRE constants traceable to NIST (CODATA 2010 set)
+CC	Iliadis 0.989534 -> CETa * 2 * pi = 0.9895106848
+CC
 
       INCLUDE 'dimension.h'
       INCLUDE 'global.h'
@@ -41,17 +39,14 @@ C     CETa = ELE2/HHBarc*SQRT(AMUmev/2)
 C     CETa = e^2 / (hbar C) SQRT( MASS*C^2 /2) = e^2 / hbar SQRT( AMUmev /2)
 C     The Sommerfeld factor eta can be calculated as CETa*SQRT(mu/EIN)*Zt*Ze
 C     eta = e^2*Zt*Ze / hbar SQRT( AMUmev*mu/ (2EIN)) where mu is adimensional, and EIN is the CMS energy in MeV
-C**      eta = CETa*Z(0)*ZEJc(0)*SQRT(mu/EIN)
-      eta = 0.989534*Z(0)*ZEJc(0)*SQRT(mu/EIN)
-      
-C     
-C**      SFACTOR = EIN*stmp*exp(2*pi*eta)
-      SFACTOR = EIN*stmp*exp(eta)
+C     eta = CETa*Z(0)*ZEJc(0)*SQRT(mu/EIN)
+	  eta = 2*pi*CETa*Z(0)*ZEJc(0)*SQRT(mu/EIN) 
 C
-C     etmp in keV
-C     etmp = EIN*1000
-C     eta = 31.29*Z(0)*ZEJc(0)*SQRT(mu/etmp)
-C     SFACTOR = EIN*stmp*exp(eta)
+C     Using the latest value of CODATA 2010 constants I obtained:
+C     CETa * 2 * pi = 0.9895106848
+C     eta = 0.989534*Z(0)*ZEJc(0)*SQRT(mu/EIN)
+
+      SFACTOR = EIN*stmp*exp(eta)
 
       WRITE(781,50) EIN, stmp, SFACTOR
    50 FORMAT(E10.4,4X,E12.6,4X,E12.6)
