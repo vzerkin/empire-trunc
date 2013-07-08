923f7b75bdfc84cbcfd007a0f034a0e5264ae2f4
Sam Hoblit <shoblit@bnl.gov>
2013-07-08 23:15:07 +0200

    svn:3446 update chkshr.sh & rmshr.sh. Only check nodes 0-7, 8-12 no longer used.

--- empy/qsubEmpire/chkshr.sh
+++ empy/qsubEmpire/chkshr.sh
@@ -17,21 +17,21 @@ echo 'On nukedata-0-3'
 ssh -x nukedata-0-3 df /dev/shm
 echo 'On nukedata-0-4'
 ssh -x nukedata-0-4 df /dev/shm
-#echo 'On nukedata-0-5'
-#ssh -x nukedata-0-5 df /dev/shm
+echo 'On nukedata-0-5'
+ssh -x nukedata-0-5 df /dev/shm
 echo 'On nukedata-0-6'
 ssh -x nukedata-0-6 df /dev/shm
 echo 'On nukedata-0-7'
 ssh -x nukedata-0-7 df /dev/shm
-echo 'On nukedata-0-8'
-ssh -x nukedata-0-8 df /dev/shm
-echo 'On nukedata-0-9'
-ssh -x nukedata-0-9 df /dev/shm
-echo 'On nukedata-0-10'
-ssh -x nukedata-0-10 df /dev/shm
-echo 'On nukedata-0-11'
-ssh -x nukedata-0-11 df /dev/shm
-echo 'On nukedata-0-12'
-ssh -x nukedata-0-12 df /dev/shm
+#echo 'On nukedata-0-8'
+#ssh -x nukedata-0-8 df /dev/shm
+#echo 'On nukedata-0-9'
+#ssh -x nukedata-0-9 df /dev/shm
+#echo 'On nukedata-0-10'
+#ssh -x nukedata-0-10 df /dev/shm
+#echo 'On nukedata-0-11'
+#ssh -x nukedata-0-11 df /dev/shm
+#echo 'On nukedata-0-12'
+#ssh -x nukedata-0-12 df /dev/shm
 
 exit
--- empy/qsubEmpire/rmshr.sh
+++ empy/qsubEmpire/rmshr.sh
@@ -16,18 +16,18 @@ rm -r /dev/shm/*
 echo ''
 
 echo ' On compute nodes:'
-ssh -x nukedata-0-0  rm -rf /dev/shm/*
-ssh -x nukedata-0-1  rm -rf /dev/shm/*
-ssh -x nukedata-0-2  rm -rf /dev/shm/*
-ssh -x nukedata-0-3  rm -rf /dev/shm/*
-ssh -x nukedata-0-4  rm -rf /dev/shm/*
-#ssh -x nukedata-0-5  rm -rf /dev/shm/*
-ssh -x nukedata-0-6  rm -rf /dev/shm/*
-ssh -x nukedata-0-7  rm -rf /dev/shm/*
-ssh -x nukedata-0-8  rm -rf /dev/shm/*
-ssh -x nukedata-0-9  rm -rf /dev/shm/*
-ssh -x nukedata-0-10 rm -rf /dev/shm/*
-ssh -x nukedata-0-11 rm -rf /dev/shm/*
-ssh -x nukedata-0-12 rm -rf /dev/shm/*
+ssh -x nukedata-0-0  rm -r /dev/shm/*
+ssh -x nukedata-0-1  rm -r /dev/shm/*
+ssh -x nukedata-0-2  rm -r /dev/shm/*
+ssh -x nukedata-0-3  rm -r /dev/shm/*
+ssh -x nukedata-0-4  rm -r /dev/shm/*
+ssh -x nukedata-0-5  rm -r /dev/shm/*
+ssh -x nukedata-0-6  rm -r /dev/shm/*
+ssh -x nukedata-0-7  rm -r /dev/shm/*
+ssh -x nukedata-0-8  rm -r /dev/shm/*
+ssh -x nukedata-0-9  rm -r /dev/shm/*
+ssh -x nukedata-0-10 rm -r /dev/shm/*
+ssh -x nukedata-0-11 rm -r /dev/shm/*
+ssh -x nukedata-0-12 rm -r /dev/shm/*
 
 exit
