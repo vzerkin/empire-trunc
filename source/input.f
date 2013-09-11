192dab512d25dae6d1445c3a0bdffb77a8ec81b0
Roberto Capote <rcapotenoy@yahoo.com>
2013-09-11 02:00:51 +0200

    svn:3503 Cosmetics

--- source/input.f
+++ source/input.f
@@ -1,6 +1,6 @@
-!cc   * $Rev: 3500 $
-!cc   * $Author: mherman $
-!cc   * $Date: 2013-09-10 18:10:23 +0200 (Di, 10 Sep 2013) $
+!cc   * $Rev: 3503 $
+!cc   * $Author: rcapote $
+!cc   * $Date: 2013-09-11 02:00:51 +0200 (Mi, 11 Sep 2013) $
       SUBROUTINE INPUT
 !cc
 !cc   ********************************************************************
@@ -2068,10 +2068,8 @@ C           NEX(nnur) = MAX(INT((EMAx(nnur)-ECUt(nnur))/DE + 1.0),0)
 C           NEXr(nejc,nnuc) = MAX(INT((emaxr-ECUt(nnur))/DE + 1.0),0)
             NEX(nnur) = MAX(INT((EMAx(nnur)-ECUt(nnur))/DE),0) 
             NEXr(nejc,nnuc) = MAX(INT((emaxr-ECUt(nnur))/DE),0) 
-!            IF(EMAx(nnur).gt.ECUt(nnur)+DE) NEX(nnur) = NEX(nnur)+1
-!            IF(emaxr.gt.ECUt(nnur)+DE) NEXr(nejc,nnuc) = NEXr(nejc,nnuc)+1
-             IF(EMAx(nnur).gt.ECUt(nnur)) NEX(nnur) = NEX(nnur)+1
-             IF(emaxr.gt.ECUt(nnur)) NEXr(nejc,nnuc) = NEXr(nejc,nnuc)+1
+            IF(EMAx(nnur).gt.ECUt(nnur)) NEX(nnur) = NEX(nnur)+1
+            IF(emaxr.gt.ECUt(nnur)) NEXr(nejc,nnuc) = NEXr(nejc,nnuc)+1
 !            IF (Nejc.eq.1 .and. nnur.eq.2)
 !     &      write(8,*) 'Ein, Nnur, Nejc, Ecut, EMAx, Emaxr, Nex, Nexr',
 !     &      Ein, Nnur, Nejc, ECUt(nnur), EMAx(nnur), emaxr, NEX(nnur),
@@ -2118,7 +2116,7 @@ C                 write(8,*) 'Z,A ',Z(nnur), A(nnur)
                   ENDDO
 C-----------------Width of the partial bin relative to DE
                   DEPart(nnur) = 1.d0 + (EX(1,nnur)-ECUt(nnur))/DE 
-!                  DEPart(nnur) = 1.d0
+!                 DEPart(nnur) = 1.d0
 !     Scaling DEPart even by 10% has a significant effect on MT=4 and 18
 !                  DEPart(nnur) = DEPart(nnur)*0.9
                   WRITE(8,
