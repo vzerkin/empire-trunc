4478c9cf5e362b4b936f42fcff31b380b98dfc30
David Brown <dbrown@bnl.gov>
2013-11-26 20:09:55 +0100

    svn:3549 update Mac's zvview (still can't get X windows working) update symlinks & wrapper

--- util/c4zvd/pickZVView.py
+++ util/c4zvd/pickZVView.py
@@ -1,6 +1,6 @@
 import os
 
 if __name__ == "__main__":
-    if os.uname()[0] == "Linux":    os.symlink( "zvv2-1.005-lin.exe", "zvview.exe" )
-    elif os.uname()[0] == "Darwin": os.symlink( "zvv2-1.005-mac.exe", "zvview.exe" )
+    if os.uname()[0] == "Linux":    os.symlink( "zvv2-1.017-lin.exe", "zvview.exe" )
+    elif os.uname()[0] == "Darwin": os.symlink( "zvv2-1.020-mac.exe", "zvview.exe" )
     else:                           os.symlink( "zvv2-1.005-win.exe", "zvview.exe" )
