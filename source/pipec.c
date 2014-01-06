702b2e4c1afcbb434760db46b4aa2250e3123404
Roberto Capote <rcapotenoy@yahoo.com>
2014-01-06 17:08:49 +0100

    svn:3711 Making svn comments to be C compatible.

--- source/pipec.c
+++ source/pipec.c
@@ -1,7 +1,6 @@
-Ccc   * $Rev: 3705 $
+/* Ccc* $Rev: 3705 $
 Ccc   * $Author: vzerkin $
-Ccc   * $Date: 2014-01-04 22:01:02 +0100 (Sat, 04 Jan 2014) $
-
+Ccc   * $Date: 2014-01-04 22:01:02 +0100 (Sat, 04 Jan 2014) $*/
 #include <stdio.h>
 #include <fcntl.h>
 
