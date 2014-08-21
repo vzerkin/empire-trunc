717de24a384c9e3b40b979b372d2b27e5fd8535b
David Brown <dbrown@bnl.gov>
2014-08-21 20:50:47 +0200

    svn:4026 forgot the driver code also fix the path to FRUIT

---
+++ source/test/fruit_driver.f90
@@ -0,0 +1,25 @@
+program fruit_driver
+  use fruit
+  use ang_mom_test
+  use HRTW_mod_test
+
+  call FCT ! initialize the factorial COMMON block used in ang_mom.f 
+    
+  call init_fruit                 
+  call test_clebsch_gordan_all_zeroes
+  call test_clebsch_gordan_odd_sum_of_js
+  call test_clebsch_gordan_m3_sumrule
+  call test_clebsch_gordan_wikipedia_values
+  call test_racah_all_zeroes
+  call test_racah_nontrivial_values_from_6j_tests
+  call test_racah_nontrivial_values_sequence
+  call test_ZCoefficient_Swave
+  call test_ZCoefficient_Pwave
+  call test_ZCoefficient_Dwave
+  call test_ZBarCoefficient_Swave
+  call test_ZBarCoefficient_Pwave
+  call test_ZBarCoefficient_Dwave
+  call test_Blatt_Biedenharn_BCoeff
+  call fruit_summary
+  call fruit_finalize
+end program fruit_driver
