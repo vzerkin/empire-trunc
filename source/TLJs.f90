4b92008395cf0db4bf0ad13b8e0f6de4ece0dd58
Mike Herman <mike.w.herman@gmail.com>
2016-03-19 22:03:38 +0100

    svn:4623 Setting up STLcc struccture for direct channels.

--- source/TLJs.f90
+++ source/TLJs.f90
@@ -1,24 +1,22 @@
-! $Rev: 4621 $
+! $Rev: 4623 $
 ! $Author: mherman $
-! $Date: 2016-03-19 19:32:07 +0100 (Sa, 19 Mär 2016) $
+! $Date: 2016-03-19 22:03:38 +0100 (Sa, 19 Mär 2016) $
 !
    MODULE TLJs
-
    IMPLICIT NONE
 
    TYPE, PUBLIC :: cc_channel
      INTEGER*4 NJcn       ! CN parity to which cc-channel couples (+1 or -1)
      REAL*8 Jcn           ! CN spin   to which cc-channel couples
+     INTEGER*4 nceq       ! Number of diagonal TLj in the P-matrix for a given CN J-pi
      INTEGER*4 lev        ! number of the collective level (in the collective level file)
      INTEGER*4 l          ! orbital angular momentum l
      REAL*8 j             ! channel spin (j in T_lj)
      REAL*8 tlj           ! Tlj value
    END TYPE cc_channel
 
-   INTEGER*4, PUBLIC :: MAX_CCch, MAX_CC
-
-   TYPE(cc_channel), PUBLIC, ALLOCATABLE, TARGET :: STLj(:) ! coupled channels for inelastic calculations (including E-W transformation)
-   
+   INTEGER*4, PUBLIC :: MAX_CCch
+   TYPE(cc_channel), PUBLIC, ALLOCATABLE, TARGET :: STLcc(:) ! coupled channels for inelastic calculations (including E-W transformation)
    PUBLIC AllocTLJs, DelTLJs 
 
    CONTAINS
@@ -26,15 +24,12 @@
    !----------------------------------------------------------------------------------------------------
 
    SUBROUTINE AllocTLJs(nch)
-
    IMPLICIT NONE
-
    INTEGER*4 nch
-
    INTEGER my
 
-   IF(allocated(STLj)) DEALLOCATE(STLj)
-   ALLOCATE(STLj(nch),STAT=my)
+   IF(allocated(STLcc)) DEALLOCATE(STLcc)
+   ALLOCATE(STLcc(nch),STAT=my)
    IF(my /= 0) THEN
      WRITE(8,*)  'ERROR: Insufficient memory for TLJs'
      WRITE(12,*) 'ERROR: Insufficient memory for TLJs'
@@ -42,25 +37,22 @@
      RETURN
    ENDIF
 
-   STLj%NJcn = 1      
-   STLj%Jcn  = 0.d0
-   STLj%lev  = 0
-   STLj%l    = 0
-   STLj%j    = 0.d0
-   STLj%tlj  = 0.d0
+   STLcc%NJcn = 1
+   STLcc%Jcn  = 0.d0
+   STLcc%nceq = 0
+   STLcc%lev  = 0
+   STLcc%l    = 0
+   STLcc%j    = 0.d0
+   STLcc%tlj  = 0.d0
 
    RETURN
-
    END SUBROUTINE AllocTLJs
 
    !----------------------------------------------------------------------------------------------------
 
    SUBROUTINE DelTLJs()
-
    IMPLICIT NONE
-
-   IF(allocated(STLj))    DEALLOCATE(STLj)
-
+   IF(allocated(STLcc))    DEALLOCATE(STLcc)
    RETURN
    END SUBROUTINE DelTLJs
 
