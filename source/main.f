0f214f04d111947597ce3f976561eb1d8bdbbac2
Gustavo Nobre <gnobre@bnl.com>
2017-08-22 17:00:02 +0200

    svn:4980 Minor change in header printing

--- source/main.f
+++ source/main.f
@@ -1,6 +1,6 @@
-cc   * $Rev: 4978 $
-Ccc   * $Author: mherman $
-Ccc   * $Date: 2017-08-21 08:05:06 +0200 (Mo, 21 Aug 2017) $
+cc   * $Rev: 4980 $
+Ccc   * $Author: gnobre $
+Ccc   * $Date: 2017-08-22 17:00:02 +0200 (Di, 22 Aug 2017) $
 
       SUBROUTINE EMPIRE
 Ccc
@@ -322,7 +322,7 @@ C           to include/exclude low-lying coupled states
      &      '  Total     ','  Elastic   ',' Nonel-Cel  ',
      &      '  Fission   ','  Mu-bar    ','  Nu-bar    ',
      &         (preaction(nnuc),nnuc=1,min(nuc_print,max_prn)),
-     &      '   (z,xa)   ','  MT=5      ','  (n,a_disc)'
+     &      '   (z,xa)   ','   MT=5     ','(n,a_disc)'
             WRITE(107,'(''#'',A10,1X,1P,20A12)')'   Einc   ',
      &      '  Total     ','  Elastic   ','     Cel    ',
      &      '   Sel      ',
