97c574d1d3c800bc38399c8f6f2e3bcb46a24f0f
Mike Herman <mike.w.herman@gmail.com>
2022-04-24 23:36:32 +0200

    svn:5334 Not needed 'import sys' removed.

--- scripts/zvd2eps.py
+++ scripts/zvd2eps.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 """
-zvd2eps.py: automatically converts all zvd files in the directory to eps format.  
+zvd2eps.py: automatically converts all zvd files in the directory to eps format.
 It corrects position of the y-axes label and can be extended to convert
-any given string in the zvd file to another one e.g., 94-Pu-239 to 239Pu. 
+any given string in the zvd file to another one e.g., 94-Pu-239 to 239Pu.
 No parameters needed.
 """
 
 
-import sys
 import glob
 import os.path
 
 def replace_string(old_string, new_string, file_to_modify):
    """
-   replace_string: Replaces all occrances of a string in a file 
+   replace_string: Replaces all occrances of a string in a file
 
    Args:
        old_string (str): string to be replaced
@@ -31,10 +30,10 @@ def replace_string(old_string, new_string, file_to_modify):
 
 # interactive = 'True'
 
-for file in glob.glob('*.zvd'): 
-   # Change a default zvd string to a nicer one below
-   # multiple lines are allowed (remove comment sign!)
-   # replace_string('92-U-233', '{+233}U', file)
+for file in glob.glob('*.zvd'):
+   # Change a default zvd string to a nicer one below.
+   # Multiple lines are allowed (remove comment sign!)
+   replace_string('92-U-233', '{+233}U', file)
    # replace_string('92-U-234', '{+234}U', file)
    # replace_string('92-U-235', '{+235}U', file)
    # replace_string('92-U-236', '{+236}U', file)
@@ -52,6 +51,4 @@ for file in glob.glob('*.zvd'):
    os.system("sed 's/^0 (/10 (/' zv.eps > zvd.eps 2>/dev/null")
    eps_file = root + '.eps'
    os.rename('zvd.eps', eps_file)
-
-
 
