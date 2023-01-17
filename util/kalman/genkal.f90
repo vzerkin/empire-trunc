ee6e0c0e2939b122875ba9ffd82cf025e65bac28
Mike Herman <mike.w.herman@gmail.com>
2023-01-17 20:44:56 +0100

    svn:5429 Cosmetics.

--- util/kalman/genkal.f90
+++ util/kalman/genkal.f90
@@ -1,27 +1,26 @@
-      PROGRAM GENKAL
+   PROGRAM GENKAL
+      !! This routine converts the empire cross sections for each reaction
+      !! and the sensitivities those reactions have for each varied empire
+      !! model parameter. The cross sections are written out to unit 50.
+      !! the sensitivies are written out to unit 52(?). This routine assumes
+      !! that the model parameters are completely independent and their
+      !! correlation matrix is the identity matrix.
 
       implicit none
 
-      ! this routine converts the empire cross sections for each reaction
-      ! and the sensitivities those reactions have for each varied empire
-      ! model parameter. The cross sections are written out to unit 50.
-      ! the sensitivies are written out to unit 52. This routine assumes
-      ! that the model parameters are completely independent and their
-      ! correlation matrix is the identity matrix.
+      integer*4, parameter :: nr = 60                !! maximum # reactions allowed
+      integer*4, parameter :: ne = 1500              !! maximum # energies allowed for each reaction
+      integer*4, parameter :: np = 200               !! maximum # of varied empire parameters
 
-      integer*4, parameter :: nr = 60                ! maximum # reactions allowed
-      integer*4, parameter :: ne = 1500              ! maximum # energies allowed for each reaction
-      integer*4, parameter :: np = 200               ! maximum # of varied empire parameters
-
-      integer*4 i,j,k,mt,mt1,ios,nreac,ie,mat,nex,npfns,l1,l2
-      integer*4 ln1,ln2,la1,la2,lb1,lb2,lc1,lc2,nparm,lt1,lt2
-      CHARACTER*4 IA,IB,IC
-      CHARACTER*6 PN,TMP
-      CHARACTER*8 INPSEN
-      CHARACTER*15 MATSEN,XSC
-      CHARACTER*12 REACTION(NR),PNAME(NP)
-      CHARACTER*25 FILE
-      real*8 E(NE),CSEC(NE,NR),PERTB(NP),SENS(NP,NE,NR),SW(NP),W(NP,NP)
+      integer*4    :: i,j,k,mt,mt1,ios,nreac,ie,mat,nex,npfns,l1,l2
+      integer*4    :: ln1,ln2,la1,la2,lb1,lb2,lc1,lc2,nparm,lt1,lt2
+      CHARACTER*4  :: IA,IB,IC
+      CHARACTER*6  :: PN,TMP
+      CHARACTER*8  :: INPSEN
+      CHARACTER*15 :: MATSEN,XSC
+      CHARACTER*12 :: REACTION(NR),PNAME(NP)
+      CHARACTER*25 :: FILE
+      real*8 :: E(NE),CSEC(NE,NR),PERTB(NP),SENS(NP,NE,NR),SW(NP),W(NP,NP)
 
       integer*4, external :: rctn
 
@@ -62,7 +61,7 @@
              stop 1
          endif
       end do
-10    CLOSE(10)
+      10 CLOSE(10)
       ie = i - 1
 
       ! CHECKING IF THE REQUIRED MT1 IS PRESENT IN THE FILE.XSC FILE
@@ -125,7 +124,7 @@
             stop 1
         endif
       end do
-20    CLOSE(10)
+      20 CLOSE(10)
       NPARM = I-1
 
       w = 0.D0
@@ -152,11 +151,12 @@
          READ(10,'(13X,A6)') PN
          CALL STRLEN(PN,LT1,LT2)
          TMP=PNAME(I)
-         WRITE(0,*) TMP(LT1:LT2),PN(LT1:LT2)
+         ! WRITE(0,*) TMP(LT1:LT2),PN(LT1:LT2)
          IF (TMP(LT1:LT2) .NE. PN(LT1:LT2)) then
             write(0,*) 'WRONG PARAMETER found in ',FILE(L1:L2)//trim(MATSEN)
             write(0,*) TMP(LT1:LT2), ' vs ',PN(LT1:LT2)
             write(0,*) 'Order in -inp.sen file might differ from -mat.sen '
+            write(0,*) 'Check also for double blank lines if sensitivities were added'
             stop 1
          endif
          READ(10,*) ! THIRD LINE
@@ -174,6 +174,6 @@
          end do
       end do
 
-100   FORMAT(11(1PE12.5))
+      100 FORMAT(11(1PE12.5))
 
-      END
+   END
